d7bb4a6a2a7e2b5b1d0a3eac3e42f3c2c8346c10
inorichi <chibilen@gmail.com>
2015-09-30 22:51:36 +0200

    Create adapter in presenter

--- app/src/main/java/eu/kanade/mangafeed/ui/adapter/LibraryAdapter.java
+++ app/src/main/java/eu/kanade/mangafeed/ui/adapter/LibraryAdapter.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2014 Ribot Ltd.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package eu.kanade.mangafeed.ui.adapter;
 
 import android.content.Context;
@@ -24,15 +9,14 @@ import java.util.List;
 import eu.kanade.mangafeed.data.models.Manga;
 import rx.Observable;
 import uk.co.ribot.easyadapter.EasyAdapter;
-import uk.co.ribot.easyadapter.ItemViewHolder;
 
-public class CatalogueArrayAdapter<T> extends EasyAdapter<T> implements Filterable {
+public class LibraryAdapter<T> extends EasyAdapter<T> implements Filterable {
 
     List<Manga> mangas;
     Filter filter;
 
-    public CatalogueArrayAdapter(Context context, Class<? extends ItemViewHolder> itemViewHolderClass, List<T> listItems) {
-        super(context, itemViewHolderClass, listItems);
+    public LibraryAdapter(Context context, List<T> listItems) {
+        super(context, MangaLibraryHolder.class, listItems);
         mangas = (List<Manga>)getItems();
         filter = new CatalogueFilter();
     }
