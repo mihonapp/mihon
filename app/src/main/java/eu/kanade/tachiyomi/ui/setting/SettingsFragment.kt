44241e03daaa53f0fd31a4f0e1243aea0f583e97
len <len@kanade.eu>
2016-11-27 22:02:23 +0100

    Update preferences lib

--- app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsFragment.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsFragment.kt
@@ -2,14 +2,10 @@ package eu.kanade.tachiyomi.ui.setting
 
 import android.os.Bundle
 import android.support.annotation.CallSuper
-import android.support.graphics.drawable.VectorDrawableCompat
-import android.support.v4.content.ContextCompat
 import android.support.v7.preference.Preference
 import android.support.v7.preference.XpPreferenceFragment
 import android.view.View
 import eu.kanade.tachiyomi.R
-import eu.kanade.tachiyomi.util.getResourceId
-import net.xpece.android.support.preference.PreferenceIconHelper
 import net.xpece.android.support.preference.PreferenceScreenNavigationStrategy
 import rx.subscriptions.CompositeSubscription
 
@@ -25,10 +21,6 @@ open class SettingsFragment : XpPreferenceFragment() {
 
     lateinit var subscriptions: CompositeSubscription
 
-    private val iconTint by lazy { ContextCompat.getColorStateList(context,
-            context.theme.getResourceId(R.attr.colorAccent, 0))
-    }
-
     override final fun onCreatePreferences2(savedState: Bundle?, rootKey: String?) {
         subscriptions = CompositeSubscription()
 
@@ -39,18 +31,6 @@ open class SettingsFragment : XpPreferenceFragment() {
         addPreferencesFromResource(R.xml.pref_sync)
         addPreferencesFromResource(R.xml.pref_advanced)
         addPreferencesFromResource(R.xml.pref_about)
-
-        // Add an icon to each subscreen
-        for ((screen, drawable) in getSubscreenIcons()) {
-            val icon = VectorDrawableCompat.create(resources, drawable, context.theme) ?: continue
-
-            PreferenceIconHelper(findPreference(screen)).apply {
-                isIconPaddingEnabled = true
-                setIcon(icon)
-                tintList = iconTint
-                isIconTintEnabled = true
-            }
-        }
 
         // Setup root preference title.
         preferenceScreen.title = activity.title
@@ -73,16 +53,6 @@ open class SettingsFragment : XpPreferenceFragment() {
         subscriptions.unsubscribe()
         super.onDestroyView()
     }
-
-    private fun getSubscreenIcons() = listOf(
-            "general_screen" to R.drawable.ic_tune_black_24dp,
-            "reader_screen" to R.drawable.ic_chrome_reader_mode_black_24dp,
-            "downloads_screen" to R.drawable.ic_file_download_black_24dp,
-            "sources_screen" to R.drawable.ic_language_black_24dp,
-            "sync_screen" to R.drawable.ic_sync_black_24dp,
-            "advanced_screen" to R.drawable.ic_code_black_24dp,
-            "about_screen" to R.drawable.ic_help_black_24dp
-    )
 
     protected inline fun <reified T : Preference> bindPref(resId: Int): Lazy<T> {
         return lazy { findPreference(getString(resId)) as T }
