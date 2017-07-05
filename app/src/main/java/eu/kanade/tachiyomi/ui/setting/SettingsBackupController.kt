fd8f7ea6938209b172cf9ebca50d4a116ae7e348
len <len@kanade.eu>
2017-07-05 14:19:58 +0200

    Fix #880. Downgrade conductor to 2.1.2 for now

--- app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsBackupController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsBackupController.kt
@@ -97,7 +97,7 @@ class SettingsBackupController : SettingsController() {
                 entriesRes = arrayOf(R.string.update_never, R.string.update_6hour,
                         R.string.update_12hour, R.string.update_24hour,
                         R.string.update_48hour, R.string.update_weekly)
-                entryValues = arrayOf("0", "6", "12", "24", "168")
+                entryValues = arrayOf("0", "6", "12", "24", "48", "168")
                 defaultValue = "0"
                 summary = "%s"
 
