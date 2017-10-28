abbe700dac40c535c27910d56aa3c29dd5708e86
inorichi <len@kanade.eu>
2017-10-28 17:01:17 +0200

    Build command and paths also changed

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,18 +3,18 @@
 git fetch --unshallow #required for commit count
 
 if [ -z "$TRAVIS_TAG" ]; then
-    ./gradlew clean buildStandardDebug
+    ./gradlew clean assembleStandardDebug
 
     COMMIT_COUNT=$(git rev-list --count HEAD)
     export ARTIFACT="tachiyomi-r${COMMIT_COUNT}.apk"
 
-    mv app/build/outputs/apk/app-standard-debug.apk $ARTIFACT
+    mv app/build/outputs/apk/standard/debug/app-standard-debug.apk $ARTIFACT
 else
-    ./gradlew clean buildStandardRelease
+    ./gradlew clean assembleStandardRelease
 
     TOOLS="${ANDROID_HOME}/build-tools/26.0.1"
     export ARTIFACT="tachiyomi-${TRAVIS_TAG}.apk"
 
-    ${TOOLS}/zipalign -v -p 4 app/build/outputs/apk/app-standard-release-unsigned app-aligned.apk
+    ${TOOLS}/zipalign -v -p 4 app/build/outputs/apk/standard/release/app-standard-release-unsigned app-aligned.apk
     ${TOOLS}/apksigner sign --ks $STORE_PATH --ks-key-alias $STORE_ALIAS --ks-pass env:STORE_PASS --key-pass env:KEY_PASS --out $ARTIFACT app-aligned.apk
 fi
