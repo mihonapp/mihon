e3259f39f18c44d40e2e845511a8680babce3fff
inorichi <len@kanade.eu>
2017-11-08 22:34:56 +0100

    Fallback covers' external directory

--- app/src/main/java/eu/kanade/tachiyomi/data/cache/CoverCache.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/cache/CoverCache.kt
@@ -20,7 +20,8 @@ class CoverCache(private val context: Context) {
     /**
      * Cache directory used for cache management.
      */
-    private val cacheDir = context.getExternalFilesDir("covers")
+    private val cacheDir = context.getExternalFilesDir("covers") ?:
+            File(context.filesDir, "covers").also { it.mkdirs() }
 
     /**
      * Returns the cover from cache.
