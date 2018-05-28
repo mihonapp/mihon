b19a4d297763acc1155f66edee83d347b4be433a
ddmgy <imapiekindaguy@gmail.com>
2018-05-28 16:54:41 -0400

    Change AniList search query to show some previously hidden entries. (#1435)

--- app/src/main/java/eu/kanade/tachiyomi/data/track/anilist/AnilistApi.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/track/anilist/AnilistApi.kt
@@ -91,7 +91,7 @@ class AnilistApi(val client: OkHttpClient, interceptor: AnilistInterceptor) {
         val query = """
             query Search(${'$'}query: String) {
                   Page (perPage: 25) {
-                    media(search: ${'$'}query, type: MANGA, format: MANGA) {
+                    media(search: ${'$'}query, type: MANGA, format_not_in: [NOVEL]) {
                       id
                       title {
                         romaji
