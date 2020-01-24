a746d4cc3a66cfd6bddaebc5ff91c865c2b7d83b
arkon <eugcheung94@gmail.com>
2020-01-24 12:18:55 -0500

    Tweak initial tracker card appearance

--- app/src/main/java/eu/kanade/tachiyomi/ui/manga/track/TrackHolder.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/manga/track/TrackHolder.kt
@@ -4,17 +4,21 @@ import android.annotation.SuppressLint
 import android.view.View
 import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.ui.base.holder.BaseViewHolder
+import eu.kanade.tachiyomi.util.visibleIf
 import kotlinx.android.synthetic.main.track_item.*
 
 class TrackHolder(view: View, adapter: TrackAdapter) : BaseViewHolder(view) {
 
     init {
         val listener = adapter.rowClickListener
+
         logo_container.setOnClickListener { listener.onLogoClick(adapterPosition) }
         title_container.setOnClickListener { listener.onTitleClick(adapterPosition) }
         status_container.setOnClickListener { listener.onStatusClick(adapterPosition) }
         chapters_container.setOnClickListener { listener.onChaptersClick(adapterPosition) }
         score_container.setOnClickListener { listener.onScoreClick(adapterPosition) }
+
+        track_set.setOnClickListener { listener.onTitleClick(adapterPosition) }
     }
 
     @SuppressLint("SetTextI18n")
@@ -23,6 +27,10 @@ class TrackHolder(view: View, adapter: TrackAdapter) : BaseViewHolder(view) {
         val track = item.track
         track_logo.setImageResource(item.service.getLogo())
         logo_container.setBackgroundColor(item.service.getLogoColor())
+
+        track_details.visibleIf { track != null }
+        track_set.visibleIf { track == null }
+
         if (track != null) {
             track_title.setTextAppearance(itemView.context, R.style.TextAppearance_Regular_Body1_Secondary)
             track_title.isAllCaps = false
@@ -31,12 +39,6 @@ class TrackHolder(view: View, adapter: TrackAdapter) : BaseViewHolder(view) {
                     if (track.total_chapters > 0) track.total_chapters else "-"
             track_status.text = item.service.getStatus(track.status)
             track_score.text = if (track.score == 0f) "-" else item.service.displayScore(track)
-        } else {
-            track_title.setTextAppearance(itemView.context, R.style.TextAppearance_Medium_Button)
-            track_title.setText(R.string.action_edit)
-            track_chapters.text = ""
-            track_score.text = ""
-            track_status.text = ""
         }
     }
 }
