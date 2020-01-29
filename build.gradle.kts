2a85bb28b9c9a7c3ab7b4fb0aae61f69b1f2c42f
arkon <eugcheung94@gmail.com>
2020-01-28 22:48:02 -0500

    Convert root level Gradle files to Kotlin DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-apply plugin: 'com.github.ben-manes.versions'
+plugins {
+    id("com.github.ben-manes.versions") version "0.27.0"
+}
 
 buildscript {
     repositories {
@@ -7,10 +9,9 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:3.5.3'
-        classpath 'com.github.ben-manes:gradle-versions-plugin:0.22.0'
-        classpath 'com.github.zellius:android-shortcut-gradle-plugin:0.1.2'
-        classpath 'com.google.gms:google-services:4.3.3'
+        classpath("com.android.tools.build:gradle:3.5.3")
+        classpath("com.github.zellius:android-shortcut-gradle-plugin:0.1.2")
+        classpath("com.google.gms:google-services:4.3.3")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
@@ -19,7 +20,11 @@ buildscript {
 allprojects {
     repositories {
         google()
-        maven { url "https://www.jitpack.io" }
+        maven { setUrl("https://www.jitpack.io") }
         jcenter()
     }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
