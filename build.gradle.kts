58ab06b4f8fed73456fd219b6b0a431a92d6a41c
Carlos <cargo8005@gmail.com>
2020-02-26 18:00:51 -0500

    add ktlint (#2633)

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:3.6.0")
         classpath("com.github.zellius:android-shortcut-gradle-plugin:0.1.2")
+        classpath("org.jmailen.gradle:kotlinter-gradle:2.3.1")
         classpath("com.google.gms:google-services:4.3.3")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -21,6 +22,7 @@ allprojects {
     repositories {
         google()
         maven { setUrl("https://www.jitpack.io") }
+        maven { setUrl("https://plugins.gradle.org/m2/") }
         jcenter()
     }
 }
