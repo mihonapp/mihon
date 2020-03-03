9eb441ac446145d1464fe13da04d7b66a917c4c2
arkon <eugcheung94@gmail.com>
2020-03-02 22:21:54 -0500

    Update versions plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.github.ben-manes.versions") version "0.27.0"
+    id("com.github.ben-manes.versions") version "0.28.0"
 }
 
 buildscript {
