446c254bc801b3f2b8f9ac19d9c2a49afe730da1
arkon <eugcheung94@gmail.com>
2020-03-15 11:56:55 -0400

    Use immersive sticky flag when hiding reader UI (closes #2705)

--- app/src/main/java/eu/kanade/tachiyomi/util/view/WindowExtensions.kt
+++ app/src/main/java/eu/kanade/tachiyomi/util/view/WindowExtensions.kt
@@ -15,7 +15,7 @@ fun Window.hideBar() {
             View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
             View.SYSTEM_UI_FLAG_FULLSCREEN or
             View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
-            View.SYSTEM_UI_FLAG_IMMERSIVE
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
     decorView.systemUiVisibility = uiFlags
 }
 
