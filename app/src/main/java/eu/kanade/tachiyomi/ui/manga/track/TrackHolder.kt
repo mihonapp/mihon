a0a88998016b82b36e65379cacbf2901db0a8d54
arkon <eugcheung94@gmail.com>
2020-04-12 22:00:19 -0400

    Tweak tracking card UI

--- app/src/main/java/eu/kanade/tachiyomi/ui/manga/track/TrackHolder.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/manga/track/TrackHolder.kt
@@ -4,16 +4,14 @@ import android.annotation.SuppressLint
 import android.view.View
 import eu.kanade.tachiyomi.ui.base.holder.BaseViewHolder
 import eu.kanade.tachiyomi.util.view.visibleIf
-import kotlinx.android.synthetic.main.track_item.chapters_container
 import kotlinx.android.synthetic.main.track_item.logo_container
-import kotlinx.android.synthetic.main.track_item.score_container
-import kotlinx.android.synthetic.main.track_item.status_container
 import kotlinx.android.synthetic.main.track_item.track_chapters
 import kotlinx.android.synthetic.main.track_item.track_details
 import kotlinx.android.synthetic.main.track_item.track_logo
 import kotlinx.android.synthetic.main.track_item.track_score
 import kotlinx.android.synthetic.main.track_item.track_set
 import kotlinx.android.synthetic.main.track_item.track_status
+import kotlinx.android.synthetic.main.track_item.track_title
 
 class TrackHolder(view: View, adapter: TrackAdapter) : BaseViewHolder(view) {
 
@@ -22,9 +20,10 @@ class TrackHolder(view: View, adapter: TrackAdapter) : BaseViewHolder(view) {
 
         logo_container.setOnClickListener { listener.onLogoClick(adapterPosition) }
         track_set.setOnClickListener { listener.onSetClick(adapterPosition) }
-        status_container.setOnClickListener { listener.onStatusClick(adapterPosition) }
-        chapters_container.setOnClickListener { listener.onChaptersClick(adapterPosition) }
-        score_container.setOnClickListener { listener.onScoreClick(adapterPosition) }
+        track_title.setOnClickListener { listener.onSetClick(adapterPosition) }
+        track_status.setOnClickListener { listener.onStatusClick(adapterPosition) }
+        track_chapters.setOnClickListener { listener.onChaptersClick(adapterPosition) }
+        track_score.setOnClickListener { listener.onScoreClick(adapterPosition) }
     }
 
     @SuppressLint("SetTextI18n")
@@ -33,8 +32,12 @@ class TrackHolder(view: View, adapter: TrackAdapter) : BaseViewHolder(view) {
         track_logo.setImageResource(item.service.getLogo())
         logo_container.setBackgroundColor(item.service.getLogoColor())
 
+        track_set.visibleIf { track == null }
+        track_title.visibleIf { track != null }
+
         track_details.visibleIf { track != null }
         if (track != null) {
+            track_title.text = track.title
             track_chapters.text = "${track.last_chapter_read}/" +
                     if (track.total_chapters > 0) track.total_chapters else "-"
             track_status.text = item.service.getStatus(track.status)
