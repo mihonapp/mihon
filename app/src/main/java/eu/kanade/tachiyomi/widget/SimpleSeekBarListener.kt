e4ff988f4dbfc985a04d414c35a90b2fb3e244c1
Jobobby04 <jobobby04@users.noreply.github.com>
2020-05-01 22:56:36 -0400

    Revert some seekbar event listener changes
    (cherry picked from commit 134dbd2f1d472cef26d4e00949769a71fc3e7e53)

---
+++ app/src/main/java/eu/kanade/tachiyomi/widget/SimpleSeekBarListener.kt
@@ -0,0 +1,14 @@
+package eu.kanade.tachiyomi.widget
+
+import android.widget.SeekBar
+
+open class SimpleSeekBarListener : SeekBar.OnSeekBarChangeListener {
+    override fun onProgressChanged(seekBar: SeekBar, value: Int, fromUser: Boolean) {
+    }
+
+    override fun onStartTrackingTouch(seekBar: SeekBar) {
+    }
+
+    override fun onStopTrackingTouch(seekBar: SeekBar) {
+    }
+}
