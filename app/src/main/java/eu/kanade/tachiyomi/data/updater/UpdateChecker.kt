63ca5cc66b83d5e484d246aac5ba982a6d0d548b
Jobobby04 <jobobby04@users.noreply.github.com>
2020-05-04 16:17:07 -0400

    Remove dev update checker

--- app/src/main/java/eu/kanade/tachiyomi/data/updater/UpdateChecker.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/updater/UpdateChecker.kt
@@ -1,18 +1,12 @@
 package eu.kanade.tachiyomi.data.updater
 
-import eu.kanade.tachiyomi.BuildConfig
-import eu.kanade.tachiyomi.data.updater.devrepo.DevRepoUpdateChecker
 import eu.kanade.tachiyomi.data.updater.github.GithubUpdateChecker
 
 abstract class UpdateChecker {
 
     companion object {
         fun getUpdateChecker(): UpdateChecker {
-            return if (BuildConfig.DEBUG) {
-                DevRepoUpdateChecker()
-            } else {
-                GithubUpdateChecker()
-            }
+            return GithubUpdateChecker()
         }
     }
 
