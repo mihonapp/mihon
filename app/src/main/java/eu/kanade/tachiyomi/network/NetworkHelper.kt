3b4b1185e22d286d3986aad7f7def58b69b0c76e
arkon <eugcheung94@gmail.com>
2020-06-23 22:26:37 -0400

    Add network header logging in debug builds

--- app/src/main/java/eu/kanade/tachiyomi/network/NetworkHelper.kt
+++ app/src/main/java/eu/kanade/tachiyomi/network/NetworkHelper.kt
@@ -1,10 +1,12 @@
 package eu.kanade.tachiyomi.network
 
 import android.content.Context
+import eu.kanade.tachiyomi.BuildConfig
 import java.io.File
 import java.util.concurrent.TimeUnit
 import okhttp3.Cache
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 
 class NetworkHelper(context: Context) {
 
@@ -14,12 +16,22 @@ class NetworkHelper(context: Context) {
 
     val cookieManager = AndroidCookieJar()
 
-    val client = OkHttpClient.Builder()
-        .cookieJar(cookieManager)
-        .cache(Cache(cacheDir, cacheSize))
-        .connectTimeout(30, TimeUnit.SECONDS)
-        .readTimeout(30, TimeUnit.SECONDS)
-        .build()
+    val client by lazy {
+        val builder = OkHttpClient.Builder()
+            .cookieJar(cookieManager)
+            .cache(Cache(cacheDir, cacheSize))
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+
+        if (BuildConfig.DEBUG) {
+            val httpLoggingInterceptor = HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.HEADERS
+            }
+            builder.addInterceptor(httpLoggingInterceptor)
+        }
+
+        builder.build()
+    }
 
     val cloudflareClient = client.newBuilder()
         .addInterceptor(UserAgentInterceptor())
