2c04c81bd1598d4dedd23ad43ef88fb16d634690
arkon <eugcheung94@gmail.com>
2020-07-25 12:07:37 -0400

    Increase dismiss timeout for what's new snackbar

--- app/src/main/java/eu/kanade/tachiyomi/ui/main/MainActivity.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/main/MainActivity.kt
@@ -403,7 +403,7 @@ class MainActivity : BaseActivity<MainActivityBinding>() {
 
         // Manually handle dismiss delay since Snackbar.LENGTH_LONG is a too short
         launchIO {
-            delay(5000)
+            delay(10000)
             snack.dismiss()
         }
     }
