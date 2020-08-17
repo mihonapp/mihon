967df6f7a3ee7cdd618027801acf7ccb4fbda6a1
arkon <eugcheung94@gmail.com>
2020-08-17 15:24:11 -0400

    Update to Kotlin 1.4

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -4,7 +4,7 @@ object Versions {
 
 object BuildPluginsVersion {
     const val AGP = "4.0.1"
-    const val KOTLIN = "1.3.72"
+    const val KOTLIN = "1.4.0"
     const val KTLINT = "9.2.1"
     const val VERSIONS_PLUGIN = "0.28.0"
 }
