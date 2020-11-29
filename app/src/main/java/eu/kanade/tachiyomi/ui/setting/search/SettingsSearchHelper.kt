3133a63cf82ac58f8731e8ff10865a516082aef0
arkon <eugcheung94@gmail.com>
2020-11-29 15:01:13 -0500

    Fix crash when a preference doesn't have a title (e.g. info preference)

--- app/src/main/java/eu/kanade/tachiyomi/ui/setting/search/SettingsSearchHelper.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/search/SettingsSearchHelper.kt
@@ -81,8 +81,8 @@ object SettingsSearchHelper {
         pref: Preference,
         breadcrumbs: String = ""
     ) {
-        when (pref) {
-            is PreferenceGroup -> {
+        when {
+            pref is PreferenceGroup -> {
                 val breadcrumbsStr = addLocalizedBreadcrumb(breadcrumbs, "${pref.title}")
 
                 for (x in 0 until pref.preferenceCount) {
@@ -90,7 +90,7 @@ object SettingsSearchHelper {
                     getSettingSearchResult(ctrl, subPref, breadcrumbsStr) // recursion
                 }
             }
-            is PreferenceCategory -> {
+            pref is PreferenceCategory -> {
                 val breadcrumbsStr = addLocalizedBreadcrumb(breadcrumbs, "${pref.title}")
 
                 for (x in 0 until pref.preferenceCount) {
@@ -98,7 +98,7 @@ object SettingsSearchHelper {
                     getSettingSearchResult(ctrl, subPref, breadcrumbsStr) // recursion
                 }
             }
-            else -> {
+            (pref.title != null) -> {
                 // Is an actual preference
                 val title = pref.title.toString()
                 val summary = if (pref.summary != null) pref.summary.toString() else ""
