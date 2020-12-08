08ab7f6aa0b6cad1fb240ca87bbdb3aec00fb8a7
arkon <eugcheung94@gmail.com>
2020-12-07 22:43:18 -0500

    Update to Kotlin 1.4.20

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,6 +1,6 @@
 object BuildPluginsVersion {
     const val AGP = "4.1.1"
-    const val KOTLIN = "1.4.10"
+    const val KOTLIN = "1.4.20"
     const val KOTLINTER = "3.0.2"
     const val VERSIONS_PLUGIN = "0.33.0"
 }
