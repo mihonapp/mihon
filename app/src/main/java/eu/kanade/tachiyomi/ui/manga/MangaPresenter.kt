2caecc01b2e1aa7ed67b2b7c57a2cbeab2aac896
arkon <eugcheung94@gmail.com>
2021-01-06 08:56:48 -0500

    Fix crash when no chapters can't be loaded

--- app/src/main/java/eu/kanade/tachiyomi/ui/manga/MangaPresenter.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/manga/MangaPresenter.kt
@@ -362,7 +362,7 @@ class MangaPresenter(
 
                 launchUI { view?.onFetchChaptersDone() }
             } catch (e: Throwable) {
-                view?.onFetchChaptersError(e)
+                launchUI { view?.onFetchChaptersError(e) }
             }
         }
     }
