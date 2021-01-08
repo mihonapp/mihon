365b849046298eca4c1e917696302026578bdb48
inorichi <len@kanade.eu>
2021-01-08 12:12:59 +0100

    Fix bad cropping of PNGs with alpha channel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -211,7 +211,7 @@ dependencies {
     implementation("com.github.bumptech.glide:okhttp3-integration:$glideVersion")
     kapt("com.github.bumptech.glide:compiler:$glideVersion")
 
-    implementation("com.github.tachiyomiorg:subsampling-scale-image-view:683808b")
+    implementation("com.github.tachiyomiorg:subsampling-scale-image-view:30691ed")
 
     // Logging
     implementation("com.jakewharton.timber:timber:4.7.1")
