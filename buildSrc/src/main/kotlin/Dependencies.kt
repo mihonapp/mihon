78e8d406492f713f66135ddfa2832cdf0731d4ac
arkon <eugcheung94@gmail.com>
2021-01-23 17:21:39 -0500

    Update AboutLibraries plugin

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -3,5 +3,5 @@ object BuildPluginsVersion {
     const val KOTLIN = "1.4.21"
     const val KOTLINTER = "3.3.0"
     const val VERSIONS_PLUGIN = "0.36.0"
-    const val ABOUTLIB_PLUGIN = "8.6.3"
+    const val ABOUTLIB_PLUGIN = "8.6.7"
 }
