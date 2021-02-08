5b7d8c5e37753aa0cd87fba22683fea6fdb5d8ed
arkon <eugcheung94@gmail.com>
2021-02-07 22:54:13 -0500

    Show locales in list of sources to migrate

--- app/src/main/java/eu/kanade/tachiyomi/ui/browse/migration/sources/SourceHolder.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/browse/migration/sources/SourceHolder.kt
@@ -1,9 +1,11 @@
 package eu.kanade.tachiyomi.ui.browse.migration.sources
 
 import android.view.View
+import androidx.core.view.isVisible
 import eu.davidea.viewholders.FlexibleViewHolder
 import eu.kanade.tachiyomi.databinding.SourceMainControllerCardItemBinding
 import eu.kanade.tachiyomi.source.icon
+import eu.kanade.tachiyomi.util.system.LocaleHelper
 
 class SourceHolder(view: View, val adapter: SourceAdapter) :
     FlexibleViewHolder(view, adapter) {
@@ -13,10 +15,10 @@ class SourceHolder(view: View, val adapter: SourceAdapter) :
     fun bind(item: SourceItem) {
         val source = item.source
 
-        // Set source name
         binding.title.text = source.name
+        binding.subtitle.isVisible = true
+        binding.subtitle.text = LocaleHelper.getDisplayName(source.lang)
 
-        // Set source icon
         itemView.post {
             binding.image.setImageDrawable(source.icon())
         }
