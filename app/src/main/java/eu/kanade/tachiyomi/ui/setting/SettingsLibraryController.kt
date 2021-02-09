aa67229dafb87e298ea5b4826bee5424754d1ee1
arkon <eugcheung94@gmail.com>
2021-02-09 17:49:02 -0500

    Add weekly to library update frequency options (closes #4422)

--- app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsLibraryController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsLibraryController.kt
@@ -129,9 +129,10 @@ class SettingsLibraryController : SettingsController() {
                     R.string.update_6hour,
                     R.string.update_12hour,
                     R.string.update_24hour,
-                    R.string.update_48hour
+                    R.string.update_48hour,
+                    R.string.update_weekly
                 )
-                entryValues = arrayOf("0", "1", "2", "3", "6", "12", "24", "48")
+                entryValues = arrayOf("0", "1", "2", "3", "6", "12", "24", "48", "168")
                 defaultValue = "24"
                 summary = "%s"
 
