d4c9ab793f3966e1e3ae17b826daf9067adca98c
inorichi <len@kanade.eu>
2021-02-23 16:53:57 +0100

    Fix a decoder crash

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -204,7 +204,7 @@ dependencies {
 
     // implementation("com.github.tachiyomiorg:subsampling-scale-image-view:6caf219")
     // TODO: switch to new decoder for stable releases
-    implementation("com.github.tachiyomiorg:subsampling-scale-image-view:a7170e9")
+    implementation("com.github.tachiyomiorg:subsampling-scale-image-view:93d74f0")
 
     // Logging
     implementation("com.jakewharton.timber:timber:4.7.1")
