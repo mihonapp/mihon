f58e3c390a014678c58c5959d14d8b55f2e11f7d
arkon <eugcheung94@gmail.com>
2021-04-06 22:29:56 -0400

    Update Kotlin and Kotlinter

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,7 +1,7 @@
 object BuildPluginsVersion {
     const val AGP = "4.1.3"
-    const val KOTLIN = "1.4.21"
-    const val KOTLINTER = "3.3.0"
+    const val KOTLIN = "1.4.32"
+    const val KOTLINTER = "3.4.0"
     const val VERSIONS_PLUGIN = "0.38.0"
     const val ABOUTLIB_PLUGIN = "8.8.4"
 }
