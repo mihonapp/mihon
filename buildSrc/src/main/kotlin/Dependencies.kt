531cd992479bbc82a4e885d04c36a0b5008ca58d
arkon <eugcheung94@gmail.com>
2021-04-10 09:48:30 -0400

    Update to Gradle 7

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -3,5 +3,5 @@ object BuildPluginsVersion {
     const val KOTLIN = "1.4.32"
     const val KOTLINTER = "3.4.0"
     const val VERSIONS_PLUGIN = "0.38.0"
-    const val ABOUTLIB_PLUGIN = "8.8.4"
+    const val ABOUTLIB_PLUGIN = "8.8.5"
 }
