27f55f809873c65fcb73724b5d9b75c9d48ef3d0
Tooster <max@polarczyk.pl>
2021-04-12 14:35:00 +0200

    Fix LibraryUpdateServiceTest so ./gradlew ... doesn't crash (#4821)

--- app/src/test/java/eu/kanade/tachiyomi/data/library/LibraryUpdateServiceTest.kt
+++ app/src/test/java/eu/kanade/tachiyomi/data/library/LibraryUpdateServiceTest.kt
@@ -98,9 +98,10 @@ class LibraryUpdateServiceTest {
         `when`(source.fetchChapterList(favManga[2])).thenReturn(Observable.just(chapters3))
 
         val intent = Intent()
+        val categoryId = intent.getIntExtra(LibraryUpdateService.KEY_CATEGORY, -1)
         val target = LibraryUpdateService.Target.CHAPTERS
         runBlocking {
-            service.addMangaToQueue(intent, target)
+            service.addMangaToQueue(categoryId, target)
             service.updateChapterList()
 
             // There are 3 network attempts and 2 insertions (1 request failed)
