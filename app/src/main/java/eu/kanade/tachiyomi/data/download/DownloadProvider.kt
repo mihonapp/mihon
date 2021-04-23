4cefbce7c34e724b409b6ba127f3c6c5c346ad8d
arkon <eugcheung94@gmail.com>
2021-04-23 08:44:12 -0400

    Make manga and chapter folder name searching case insensitive

--- app/src/main/java/eu/kanade/tachiyomi/data/download/DownloadProvider.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/download/DownloadProvider.kt
@@ -76,7 +76,7 @@ class DownloadProvider(private val context: Context) {
      */
     fun findMangaDir(manga: Manga, source: Source): UniFile? {
         val sourceDir = findSourceDir(source)
-        return sourceDir?.findFile(getMangaDirName(manga))
+        return sourceDir?.findFile(getMangaDirName(manga), true)
     }
 
     /**
@@ -89,7 +89,7 @@ class DownloadProvider(private val context: Context) {
     fun findChapterDir(chapter: Chapter, manga: Manga, source: Source): UniFile? {
         val mangaDir = findMangaDir(manga, source)
         return getValidChapterDirNames(chapter).asSequence()
-            .mapNotNull { mangaDir?.findFile(it) }
+            .mapNotNull { mangaDir?.findFile(it, true) }
             .firstOrNull()
     }
 
@@ -150,6 +150,7 @@ class DownloadProvider(private val context: Context) {
         return listOf(
             getChapterDirName(chapter),
 
+            // TODO: remove this
             // Legacy chapter directory name used in v0.9.2 and before
             DiskUtil.buildValidFilename(chapter.name)
         )
