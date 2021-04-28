6a4c280235409fd53fdcb45842a8f5cb364ee51d
Jays2Kings <jays@outlook.com>
2021-04-28 04:01:05 -0400

    Change string chop method default to use smaller ... instead
    (cherry picked from commit 43b42f8d54bf8872b4fd2467b33fa06a9a44dbb6)

--- app/src/main/java/eu/kanade/tachiyomi/util/lang/StringExtensions.kt
+++ app/src/main/java/eu/kanade/tachiyomi/util/lang/StringExtensions.kt
@@ -7,7 +7,7 @@ import kotlin.math.floor
  * Replaces the given string to have at most [count] characters using [replacement] at its end.
  * If [replacement] is longer than [count] an exception will be thrown when `length > count`.
  */
-fun String.chop(count: Int, replacement: String = "..."): String {
+fun String.chop(count: Int, replacement: String = "…"): String {
     return if (length > count) {
         take(count - replacement.length) + replacement
     } else {
