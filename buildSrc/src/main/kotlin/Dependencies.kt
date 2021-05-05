998da965cddc8518171840fd9773c254f0515bee
arkon <eugcheung94@gmail.com>
2021-05-04 22:44:34 -0400

    Update AGP for Android Studio 4.2

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,5 +1,5 @@
 object BuildPluginsVersion {
-    const val AGP = "4.1.3"
+    const val AGP = "4.2.0"
     const val KOTLIN = "1.4.32"
     const val KOTLINTER = "3.4.0"
     const val VERSIONS_PLUGIN = "0.38.0"
