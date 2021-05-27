c57d0046bc99cf378457f7977ec029e59bc8ffc5
arkon <eugcheung94@gmail.com>
2021-05-27 18:13:11 -0400

    Update GMS plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ subprojects {
 buildscript {
     dependencies {
         classpath("com.github.zellius:android-shortcut-gradle-plugin:0.1.2")
-        classpath("com.google.gms:google-services:4.3.5")
+        classpath("com.google.gms:google-services:4.3.8")
         classpath("com.mikepenz.aboutlibraries.plugin:aboutlibraries-plugin:${BuildPluginsVersion.ABOUTLIB_PLUGIN}")
         classpath(kotlin("serialization", version = BuildPluginsVersion.KOTLIN))
     }
