bd424ce460bb9b569a6c7ca37151cbbf5e2e6ee8
arkon <eugcheung94@gmail.com>
2021-07-14 17:53:42 -0400

    Update to Kotlin 1.5.21

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,6 +1,6 @@
 object BuildPluginsVersion {
     const val AGP = "4.2.2"
-    const val KOTLIN = "1.5.20"
+    const val KOTLIN = "1.5.21"
     const val KOTLINTER = "3.4.5"
     const val VERSIONS_PLUGIN = "0.39.0"
     const val ABOUTLIB_PLUGIN = "8.9.0"
