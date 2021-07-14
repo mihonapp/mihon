117fd4bd0fe0be3d1f14f8fba736a813332730a0
arkon <eugcheung94@gmail.com>
2021-07-14 17:59:09 -0400

    Chop long titles in library update notification, fix incrementing when updating covers

--- app/src/main/java/eu/kanade/tachiyomi/data/library/LibraryUpdateService.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/library/LibraryUpdateService.kt
@@ -416,7 +416,6 @@ class LibraryUpdateService(
                                 }
 
                                 currentlyUpdatingManga.add(manga)
-                                progressCount.andIncrement
                                 notifier.showProgressNotification(
                                     currentlyUpdatingManga,
                                     progressCount.get(),
@@ -440,6 +439,7 @@ class LibraryUpdateService(
                                 }
 
                                 currentlyUpdatingManga.remove(manga)
+                                progressCount.andIncrement
                                 notifier.showProgressNotification(
                                     currentlyUpdatingManga,
                                     progressCount.get(),
