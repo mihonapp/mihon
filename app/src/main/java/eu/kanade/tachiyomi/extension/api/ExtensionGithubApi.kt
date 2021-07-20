4acc7cee3d84ca36389e47e47868d57941c887f0
arkon <eugcheung94@gmail.com>
2021-07-20 17:23:55 -0400

    Revert jsDelivr CDN fallback
    It doesn't work unless you provide actual semver versions, but we don't do that.

--- app/src/main/java/eu/kanade/tachiyomi/extension/api/ExtensionGithubApi.kt
+++ app/src/main/java/eu/kanade/tachiyomi/extension/api/ExtensionGithubApi.kt
@@ -22,23 +22,11 @@ internal class ExtensionGithubApi {
     private val networkService: NetworkHelper by injectLazy()
     private val preferences: PreferencesHelper by injectLazy()
 
-    private var requiresFallbackSource = false
-
     suspend fun findExtensions(): List<Extension.Available> {
         return withIOContext {
-            val response = try {
-                networkService.client
-                    .newCall(GET("${REPO_URL_PREFIX}index.min.json"))
-                    .await()
-            } catch (e: Throwable) {
-                requiresFallbackSource = true
-
-                networkService.client
-                    .newCall(GET("${FALLBACK_REPO_URL_PREFIX}index.min.json"))
-                    .await()
-            }
-
-            response
+            networkService.client
+                .newCall(GET("${REPO_URL_PREFIX}index.min.json"))
+                .await()
                 .parseAs<JsonArray>()
                 .let { parseResponse(it) }
         }
@@ -82,23 +70,15 @@ internal class ExtensionGithubApi {
                 val versionCode = element.jsonObject["code"]!!.jsonPrimitive.int
                 val lang = element.jsonObject["lang"]!!.jsonPrimitive.content
                 val nsfw = element.jsonObject["nsfw"]!!.jsonPrimitive.int == 1
-                val icon = "${getUrlPrefix()}icon/${apkName.replace(".apk", ".png")}"
+                val icon = "${REPO_URL_PREFIX}icon/${apkName.replace(".apk", ".png")}"
 
                 Extension.Available(name, pkgName, versionName, versionCode, lang, nsfw, apkName, icon)
             }
     }
 
     fun getApkUrl(extension: Extension.Available): String {
-        return "${getUrlPrefix()}apk/${extension.apkName}"
-    }
-
-    private fun getUrlPrefix(): String {
-        return when (requiresFallbackSource) {
-            true -> FALLBACK_REPO_URL_PREFIX
-            false -> REPO_URL_PREFIX
-        }
+        return "${REPO_URL_PREFIX}apk/${extension.apkName}"
     }
 }
 
 private const val REPO_URL_PREFIX = "https://raw.githubusercontent.com/tachiyomiorg/tachiyomi-extensions/repo/"
-private const val FALLBACK_REPO_URL_PREFIX = "https://cdn.jsdelivr.net/gh/tachiyomiorg/tachiyomi-extensions@repo/"
