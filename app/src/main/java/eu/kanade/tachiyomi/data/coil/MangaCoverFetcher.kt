mihonapp/mihon

commit 93e61367958e280689423b7692cd98cda59805e9

Use Coil (#4870)
* Use Coil

* Remove coil-transformations lib

* Add MangaCoverFetcher

* Remove Glide

* MangaCoverFetcher: Allow skipping custom cover usage

* Adjust coil caching policy for some non-library items

* Allow coil to use RGB565 only on low ram devices

* Fix image loading progress view not showing

a

* Increase coil crossfade duration

Same as default glide duration

* Add back request clearing

app/src/main/java/eu/kanade/tachiyomi/data/coil/MangaCoverFetcher.kt
@@ -1,0 +1,172 @@
+package eu.kanade.tachiyomi.data.coil
+
+import coil.bitmap.BitmapPool
+import coil.decode.DataSource
+import coil.decode.Options
+import coil.fetch.FetchResult
+import coil.fetch.Fetcher
+import coil.fetch.SourceResult
+import coil.network.HttpException
+import coil.request.get
+import coil.size.Size
+import eu.kanade.tachiyomi.data.cache.CoverCache
+import eu.kanade.tachiyomi.data.database.models.Manga
+import eu.kanade.tachiyomi.network.NetworkHelper
+import eu.kanade.tachiyomi.network.await
+import eu.kanade.tachiyomi.source.SourceManager
+import eu.kanade.tachiyomi.source.online.HttpSource
+import okhttp3.CacheControl
+import okhttp3.Call
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.ResponseBody
+import okio.buffer
+import okio.sink
+import okio.source
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
+import uy.kohesive.injekt.injectLazy
+import java.io.File
+import java.util.Date
+
+/**
+ * Coil component that fetches [Manga] cover while using the cached file in disk when available.
+ *
+ * Available request parameter:
+ * - [USE_CUSTOM_COVER]: Use custom cover if set by user, default is true
+ */
+class MangaCoverFetcher : Fetcher<Manga> {
+    private val coverCache: CoverCache by injectLazy()
+    private val sourceManager: SourceManager by injectLazy()
+    private val defaultClient = Injekt.get<NetworkHelper>().coilClient
+
+    override fun key(data: Manga): String? {
+        if (data.thumbnail_url.isNullOrBlank()) return null
+        return data.thumbnail_url!!
+    }
+
+    override suspend fun fetch(pool: BitmapPool, data: Manga, size: Size, options: Options): FetchResult {
+        // Use custom cover if exists
+        val useCustomCover = options.parameters[USE_CUSTOM_COVER] as? Boolean ?: true
+        val customCoverFile = coverCache.getCustomCoverFile(data)
+        if (useCustomCover && customCoverFile.exists()) {
+            return fileLoader(customCoverFile)
+        }
+
+        val cover = data.thumbnail_url
+        return when (getResourceType(cover)) {
+            Type.URL -> httpLoader(data, options)
+            Type.File -> fileLoader(data)
+            null -> error("Invalid image")
+        }
+    }
+
+    private suspend fun httpLoader(manga: Manga, options: Options): FetchResult {
+        val coverFile = coverCache.getCoverFile(manga) ?: error("No cover specified")
+
+        // Use previously cached cover if exist
+        if (coverFile.exists() && options.diskCachePolicy.readEnabled) {
+            if (!manga.favorite) {
+                coverFile.setLastModified(Date().time)
+            }
+            return fileLoader(coverFile)
+        }
+
+        val (response, body) = awaitGetCall(manga, options)
+        if (!response.isSuccessful) {
+            body.close()
+            throw HttpException(response)
+        }
+
+        // Write to disk for future use
+        if (options.diskCachePolicy.writeEnabled) {
+            response.peekBody(Long.MAX_VALUE).source().use { input ->
+                val tmpFile = File(coverFile.absolutePath + "_tmp")
+                tmpFile.parentFile?.mkdirs()
+                tmpFile.sink().buffer().use { output ->
+                    output.writeAll(input)
+                }
+                if (coverFile.exists()) {
+                    coverFile.delete()
+                }
+                tmpFile.renameTo(coverFile)
+            }
+        }
+
+        return SourceResult(
+            source = body.source(),
+            mimeType = "image/*",
+            dataSource = if (response.cacheResponse != null) DataSource.DISK else DataSource.NETWORK
+        )
+    }
+
+    private suspend fun awaitGetCall(manga: Manga, options: Options): Pair<Response, ResponseBody> {
+        val call = getCall(manga, options)
+        val response = call.await()
+        return response to checkNotNull(response.body) { "Null response source" }
+    }
+
+    private fun getCall(manga: Manga, options: Options): Call {
+        val source = sourceManager.get(manga.source) as? HttpSource
+        val client = source?.client ?: defaultClient
+
+        val newClient = client.newBuilder().build()
+
+        val request = Request.Builder().url(manga.thumbnail_url!!).also {
+            if (source != null) {
+                it.headers(source.headers)
+            }
+
+            val networkRead = options.networkCachePolicy.readEnabled
+            val diskRead = options.diskCachePolicy.readEnabled
+            when {
+                !networkRead && diskRead -> {
+                    it.cacheControl(CacheControl.FORCE_CACHE)
+                }
+                networkRead && !diskRead -> if (options.diskCachePolicy.writeEnabled) {
+                    it.cacheControl(CacheControl.FORCE_NETWORK)
+                } else {
+                    it.cacheControl(CACHE_CONTROL_FORCE_NETWORK_NO_CACHE)
+                }
+                !networkRead && !diskRead -> {
+                    // This causes the request to fail with a 504 Unsatisfiable Request.
+                    it.cacheControl(CACHE_CONTROL_NO_NETWORK_NO_CACHE)
+                }
+            }
+        }.build()
+
+        return newClient.newCall(request)
+    }
+
+    private fun fileLoader(manga: Manga): FetchResult {
+        return fileLoader(File(manga.thumbnail_url!!.substringAfter("file://")))
+    }
+
+    private fun fileLoader(file: File): FetchResult {
+        return SourceResult(
+            source = file.source().buffer(),
+            mimeType = "image/*",
+            dataSource = DataSource.DISK
+        )
+    }
+
+    private fun getResourceType(cover: String?): Type? {
+        return when {
+            cover.isNullOrEmpty() -> null
+            cover.startsWith("http") || cover.startsWith("Custom-", true) -> Type.URL
+            cover.startsWith("/") || cover.startsWith("file://") -> Type.File
+            else -> null
+        }
+    }
+
+    private enum class Type {
+        File, URL
+    }
+
+    companion object {
+        const val USE_CUSTOM_COVER = "use_custom_cover"
+
+        private val CACHE_CONTROL_FORCE_NETWORK_NO_CACHE = CacheControl.Builder().noCache().noStore().build()
+        private val CACHE_CONTROL_NO_NETWORK_NO_CACHE = CacheControl.Builder().noCache().onlyIfCached().build()
+    }
+}

app/build.gradle.kts
@@ -198,10 +198,9 @@ dependencies {
     implementation("com.github.inorichi.injekt:injekt-core:65b0440")
 
     // Image library
-    val glideVersion = "4.12.0"
-    implementation("com.github.bumptech.glide:glide:$glideVersion")
-    implementation("com.github.bumptech.glide:okhttp3-integration:$glideVersion")
-    kapt("com.github.bumptech.glide:compiler:$glideVersion")
+    val coilVersion = "1.2.0"
+    implementation("io.coil-kt:coil:$coilVersion")
+    implementation("io.coil-kt:coil-gif:$coilVersion")
 
     implementation("com.github.tachiyomiorg:subsampling-scale-image-view:547d9c0")
 
@@ -278,7 +277,8 @@ tasks {
             "-Xuse-experimental=kotlinx.coroutines.FlowPreview",
             "-Xuse-experimental=kotlinx.coroutines.ExperimentalCoroutinesApi",
             "-Xuse-experimental=kotlinx.coroutines.InternalCoroutinesApi",
-            "-Xuse-experimental=kotlinx.serialization.ExperimentalSerializationApi"
+            "-Xuse-experimental=kotlinx.serialization.ExperimentalSerializationApi",
+            "-Xuse-experimental=coil.annotation.ExperimentalCoilApi",
         )
     }
 

app/src/main/java/eu/kanade/tachiyomi/App.kt
@@ -1,16 +1,25 @@
 package eu.kanade.tachiyomi
 
+import android.app.ActivityManager
 import android.app.Application
 import android.content.Context
 import android.content.res.Configuration
 import android.os.Build
+import androidx.core.content.getSystemService
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.OnLifecycleEvent
 import androidx.lifecycle.ProcessLifecycleOwner
 import androidx.multidex.MultiDex
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
+import eu.kanade.tachiyomi.data.coil.ByteBufferFetcher
+import eu.kanade.tachiyomi.data.coil.MangaCoverFetcher
 import eu.kanade.tachiyomi.data.notification.Notifications
 import eu.kanade.tachiyomi.data.preference.PreferencesHelper
+import eu.kanade.tachiyomi.network.NetworkHelper
 import eu.kanade.tachiyomi.ui.security.SecureActivityDelegate
 import eu.kanade.tachiyomi.util.system.LocaleHelper
 import org.acra.ACRA
@@ -20,6 +29,7 @@ import org.acra.sender.HttpSender
 import org.conscrypt.Conscrypt
 import timber.log.Timber
 import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
 import uy.kohesive.injekt.injectLazy
 import java.security.Security
 
@@ -31,7 +41,7 @@ import java.security.Security
     uri = BuildConfig.ACRA_URI,
     httpMethod = HttpSender.Method.PUT
 )
-open class App : Application(), LifecycleObserver {
+open class App : Application(), LifecycleObserver, ImageLoaderFactory {
 
     private val preferences: PreferencesHelper by injectLazy()
 
@@ -65,6 +75,23 @@ open class App : Application(), LifecycleObserver {
     override fun onConfigurationChanged(newConfig: Configuration) {
         super.onConfigurationChanged(newConfig)
         LocaleHelper.updateConfiguration(this, newConfig, true)
+    }
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this).apply {
+            componentRegistry {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                    add(ImageDecoderDecoder(this@App))
+                } else {
+                    add(GifDecoder())
+                }
+                add(ByteBufferFetcher())
+                add(MangaCoverFetcher())
+            }
+            okHttpClient(Injekt.get<NetworkHelper>().coilClient)
+            crossfade(300)
+            allowRgb565(getSystemService<ActivityManager>()!!.isLowRamDevice)
+        }.build()
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_STOP)

app/src/main/java/eu/kanade/tachiyomi/data/cache/CoverCache.kt
@@ -1,6 +1,7 @@
 package eu.kanade.tachiyomi.data.cache
 
 import android.content.Context
+import coil.imageLoader
 import eu.kanade.tachiyomi.data.database.models.Manga
 import eu.kanade.tachiyomi.util.storage.DiskUtil
 import java.io.File
@@ -97,6 +98,13 @@ class CoverCache(private val context: Context) {
         return getCustomCoverFile(manga).let {
             it.exists() && it.delete()
         }
+    }
+
+    /**
+     * Clear coil's memory cache.
+     */
+    fun clearMemoryCache() {
+        context.imageLoader.memoryCache.clear()
     }
 
     private fun getCacheDir(dir: String): File {

app/src/main/java/eu/kanade/tachiyomi/data/coil/ByteBufferFetcher.kt
@@ -1,0 +1,25 @@
+package eu.kanade.tachiyomi.data.coil
+
+import coil.bitmap.BitmapPool
+import coil.decode.DataSource
+import coil.decode.Options
+import coil.fetch.FetchResult
+import coil.fetch.Fetcher
+import coil.fetch.SourceResult
+import coil.size.Size
+import okio.buffer
+import okio.source
+import java.io.ByteArrayInputStream
+import java.nio.ByteBuffer
+
+class ByteBufferFetcher : Fetcher<ByteBuffer> {
+    override suspend fun fetch(pool: BitmapPool, data: ByteBuffer, size: Size, options: Options): FetchResult {
+        return SourceResult(
+            source = ByteArrayInputStream(data.array()).source().buffer(),
+            mimeType = null,
+            dataSource = DataSource.MEMORY
+        )
+    }
+
+    override fun key(data: ByteBuffer): String? = null
+}

app/src/main/java/eu/kanade/tachiyomi/data/glide/FileFetcher.kt
@@ -1,60 +1,0 @@
-package eu.kanade.tachiyomi.data.glide
-
-import android.content.ContentValues.TAG
-import android.util.Log
-import com.bumptech.glide.Priority
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.data.DataFetcher
-import timber.log.Timber
-import java.io.File
-import java.io.FileInputStream
-import java.io.FileNotFoundException
-import java.io.IOException
-import java.io.InputStream
-
-open class FileFetcher(private val filePath: String = "") : DataFetcher<InputStream> {
-
-    private var data: InputStream? = null
-
-    override fun loadData(priority: Priority, callback: DataFetcher.DataCallback<in InputStream>) {
-        loadFromFile(callback)
-    }
-
-    private fun loadFromFile(callback: DataFetcher.DataCallback<in InputStream>) {
-        loadFromFile(File(filePath), callback)
-    }
-
-    protected fun loadFromFile(file: File, callback: DataFetcher.DataCallback<in InputStream>) {
-        try {
-            data = FileInputStream(file)
-        } catch (e: FileNotFoundException) {
-            if (Log.isLoggable(TAG, Log.DEBUG)) {
-                Timber.d(e, "Failed to open file")
-            }
-            callback.onLoadFailed(e)
-            return
-        }
-
-        callback.onDataReady(data)
-    }
-
-    override fun cleanup() {
-        try {
-            data?.close()
-        } catch (e: IOException) {
-            // Ignored.
-        }
-    }
-
-    override fun cancel() {
-        // Do nothing.
-    }
-
-    override fun getDataClass(): Class<InputStream> {
-        return InputStream::class.java
-    }
-
-    override fun getDataSource(): DataSource {
-        return DataSource.LOCAL
-    }
-}

app/src/main/java/eu/kanade/tachiyomi/data/glide/LibraryMangaCustomCoverFetcher.kt
@@ -1,25 +1,0 @@
-package eu.kanade.tachiyomi.data.glide
-
-import com.bumptech.glide.Priority
-import com.bumptech.glide.load.data.DataFetcher
-import eu.kanade.tachiyomi.data.cache.CoverCache
-import eu.kanade.tachiyomi.data.database.models.Manga
-import java.io.File
-import java.io.InputStream
-import java.lang.Exception
-
-open class LibraryMangaCustomCoverFetcher(
-    private val manga: Manga,
-    private val coverCache: CoverCache
-) : FileFetcher() {
-
-    override fun loadData(priority: Priority, callback: DataFetcher.DataCallback<in InputStream>) {
-        getCustomCoverFile()?.let {
-            loadFromFile(it, callback)
-        } ?: callback.onLoadFailed(Exception("Custom cover file not found"))
-    }
-
-    protected fun getCustomCoverFile(): File? {
-        return coverCache.getCustomCoverFile(manga).takeIf { it.exists() }
-    }
-}

app/src/main/java/eu/kanade/tachiyomi/data/glide/LibraryMangaUrlFetcher.kt
@@ -1,86 +1,0 @@
-package eu.kanade.tachiyomi.data.glide
-
-import com.bumptech.glide.Priority
-import com.bumptech.glide.load.data.DataFetcher
-import eu.kanade.tachiyomi.data.cache.CoverCache
-import eu.kanade.tachiyomi.data.database.models.Manga
-import java.io.File
-import java.io.FileNotFoundException
-import java.io.InputStream
-
-/**
- * A [DataFetcher] for loading a cover of a library manga.
- * It tries to load the cover from our custom cache, and if it's not found, it fallbacks to network
- * and copies the result to the cache.
- *
- * @param networkFetcher the network fetcher for this cover.
- * @param manga the manga of the cover to load.
- * @param file the file where this cover should be. It may exists or not.
- */
-class LibraryMangaUrlFetcher(
-    private val networkFetcher: DataFetcher<InputStream>,
-    private val manga: Manga,
-    private val coverCache: CoverCache
-) : LibraryMangaCustomCoverFetcher(manga, coverCache) {
-
-    override fun loadData(priority: Priority, callback: DataFetcher.DataCallback<in InputStream>) {
-        getCustomCoverFile()?.let {
-            loadFromFile(it, callback)
-            return
-        }
-
-        val cover = coverCache.getCoverFile(manga)
-        if (cover == null) {
-            callback.onLoadFailed(Exception("Null thumbnail url"))
-            return
-        }
-
-        if (!cover.exists()) {
-            networkFetcher.loadData(
-                priority,
-                object : DataFetcher.DataCallback<InputStream> {
-                    override fun onDataReady(data: InputStream?) {
-                        if (data != null) {
-                            val tmpFile = File(cover.path + ".tmp")
-                            try {
-                                // Retrieve destination stream, create parent folders if needed.
-                                val output = try {
-                                    tmpFile.outputStream()
-                                } catch (e: FileNotFoundException) {
-                                    tmpFile.parentFile!!.mkdirs()
-                                    tmpFile.outputStream()
-                                }
-
-                                // Copy the file and rename to the original.
-                                data.use { output.use { data.copyTo(output) } }
-                                tmpFile.renameTo(cover)
-                                loadFromFile(cover, callback)
-                            } catch (e: Exception) {
-                                tmpFile.delete()
-                                callback.onLoadFailed(e)
-                            }
-                        } else {
-                            callback.onLoadFailed(Exception("Null data"))
-                        }
-                    }
-
-                    override fun onLoadFailed(e: Exception) {
-                        callback.onLoadFailed(e)
-                    }
-                }
-            )
-        } else {
-            loadFromFile(cover, callback)
-        }
-    }
-
-    override fun cleanup() {
-        super.cleanup()
-        networkFetcher.cleanup()
-    }
-
-    override fun cancel() {
-        super.cancel()
-        networkFetcher.cancel()
-    }
-}

commit 20264eecb9c0e583b8ffc10f9437e8b3797bb0f1

fix regression in cover fetching (#5621)
introduced in https://github.com/tachiyomiorg/tachiyomi/pull/4870
this was previously done here: https://github.com/tachiyomiorg/tachiyomi/blob/7d23fd8ef5bbb9efd12f65bb5c9670464dd6c0b1/app/src/main/java/eu/kanade/tachiyomi/data/glide/MangaThumbnailModelLoader.kt#L95 as part of https://github.com/tachiyomiorg/tachiyomi/pull/2908

this impacts Komga extension, where server url is manually provided by the user, but due to auto-correct on mobile keyboards the `http` prefix sometimes get capitalized to `Http`.

app/src/main/java/eu/kanade/tachiyomi/data/coil/MangaCoverFetcher.kt
@@ -148,7 +148,7 @@ class MangaCoverFetcher : Fetcher<Manga> {
     private fun getResourceType(cover: String?): Type? {
         return when {
             cover.isNullOrEmpty() -> null
-            cover.startsWith("http") || cover.startsWith("Custom-", true) -> Type.URL
+            cover.startsWith("http", true) || cover.startsWith("Custom-", true) -> Type.URL
             cover.startsWith("/") || cover.startsWith("file://") -> Type.File
             else -> null
         }