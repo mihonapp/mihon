a3090e62f5491d1a50bb97206dc5534e086959db
Ivan Iskandar <12537387+ivaniskandar@users.noreply.github.com>
2021-07-31 05:13:50 +0700

    Fix reader activity not using preferred language (#5630)

--- app/src/main/java/eu/kanade/tachiyomi/ui/base/activity/BaseViewBindingActivity.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/base/activity/BaseViewBindingActivity.kt
@@ -1,10 +1,8 @@
 package eu.kanade.tachiyomi.ui.base.activity
 
-import android.content.Context
 import android.os.Bundle
 import androidx.viewbinding.ViewBinding
 import eu.kanade.tachiyomi.ui.security.SecureActivityDelegate
-import eu.kanade.tachiyomi.util.system.LocaleHelper
 
 abstract class BaseViewBindingActivity<VB : ViewBinding> : BaseThemedActivity() {
 
@@ -12,10 +10,6 @@ abstract class BaseViewBindingActivity<VB : ViewBinding> : BaseThemedActivity() 
 
     @Suppress("LeakingThis")
     private val secureActivityDelegate = SecureActivityDelegate(this)
-
-    override fun attachBaseContext(newBase: Context) {
-        super.attachBaseContext(LocaleHelper.createLocaleWrapper(newBase))
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
