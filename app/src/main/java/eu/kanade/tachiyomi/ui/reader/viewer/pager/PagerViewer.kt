26b8df5354ad6d8a6e147801de009ac3e77d028d
Ivan Iskandar <12537387+ivaniskandar@users.noreply.github.com>
2021-08-19 22:53:26 +0700

    Partial revert 914b686c8eaf3954e291355975532eb28662c38c (#5749)
    Didn't mean to remove this line, this fixes resuming to read downloaded chapter.

--- app/src/main/java/eu/kanade/tachiyomi/ui/reader/viewer/pager/PagerViewer.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/reader/viewer/pager/PagerViewer.kt
@@ -77,6 +77,7 @@ abstract class PagerViewer(val activity: ReaderActivity) : BaseViewer {
         pager.layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
         pager.offscreenPageLimit = 1
         pager.id = R.id.reader_pager
+        pager.adapter = adapter
         pager.addOnPageChangeListener(
             object : ViewPager.SimpleOnPageChangeListener() {
                 override fun onPageSelected(position: Int) {
