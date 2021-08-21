7d2caeb27060e8f14c2192f6e7a8931657976e2c
arkon <eugcheung94@gmail.com>
2021-08-20 22:42:16 -0400

    Minor cleanup

--- app/src/main/java/eu/kanade/tachiyomi/ui/browse/source/browse/BrowseSourcePresenter.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/browse/source/browse/BrowseSourcePresenter.kt
@@ -38,7 +38,6 @@ import eu.kanade.tachiyomi.util.lang.launchIO
 import eu.kanade.tachiyomi.util.lang.withUIContext
 import eu.kanade.tachiyomi.util.removeCovers
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
@@ -90,11 +89,6 @@ open class BrowseSourcePresenter(
      * Pager containing a list of manga results.
      */
     private lateinit var pager: Pager
-
-    /**
-     * Flow of manga list to initialize.
-     */
-    private val mangaDetailsFlow = MutableStateFlow<List<Manga>>(emptyList())
 
     /**
      * Subscription for the pager.
