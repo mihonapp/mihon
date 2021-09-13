3eec20716644f1d69f13123fb435c6f9d6eeff16
arkon <eugcheung94@gmail.com>
2021-09-13 15:10:41 -0400

    Release v0.12.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         minSdk = AndroidConfig.minSdk
         targetSdk = AndroidConfig.targetSdk
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        versionCode = 67
-        versionName = "0.12.1"
+        versionCode = 68
+        versionName = "0.12.2"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
