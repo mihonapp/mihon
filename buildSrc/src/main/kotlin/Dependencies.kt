90c9f28818f9e1c3029649d3ba9e97301b39ccba
arkon <eugcheung94@gmail.com>
2021-10-13 09:15:46 -0400

    Update AGP

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,5 +1,5 @@
 object BuildPluginsVersion {
-    const val AGP = "7.0.2"
+    const val AGP = "7.0.3"
     const val KOTLIN = "1.5.31"
     const val KOTLINTER = "3.6.0"
     const val VERSIONS_PLUGIN = "0.39.0"
