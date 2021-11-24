6cfd2c510b9ee15bc393db44ad4adce0dfc5c915
arkon <eugcheung94@gmail.com>
2021-11-24 17:34:39 -0500

    Fix crash in clear database screen (fixes #6271)

--- app/src/main/java/eu/kanade/tachiyomi/ui/setting/database/ClearDatabasePresenter.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/database/ClearDatabasePresenter.kt
@@ -5,6 +5,7 @@ import eu.kanade.tachiyomi.data.database.DatabaseHelper
 import eu.kanade.tachiyomi.source.SourceManager
 import eu.kanade.tachiyomi.ui.base.presenter.BasePresenter
 import rx.Observable
+import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
 import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
@@ -19,6 +20,7 @@ class ClearDatabasePresenter : BasePresenter<ClearDatabaseController>() {
         super.onCreate(savedState)
         getDatabaseSourcesObservable()
             .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
             .subscribeLatestCache(ClearDatabaseController::setItems)
     }
 
