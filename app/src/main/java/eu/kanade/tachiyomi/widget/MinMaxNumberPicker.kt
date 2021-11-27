3463d6c752cfc1145f4c8db9f65f145002ece5fb
Ivan Iskandar <12537387+ivaniskandar@users.noreply.github.com>
2021-11-28 00:48:49 +0700

    MinMaxNumberPicker: Set IME input to use number only (#6286)
    * MinMaxNumberPicker: Set IME input to use number only
    
    * MinMaxNumberPicker: Auto disable keyboard input when needed

--- app/src/main/java/eu/kanade/tachiyomi/widget/MinMaxNumberPicker.kt
+++ app/src/main/java/eu/kanade/tachiyomi/widget/MinMaxNumberPicker.kt
@@ -1,12 +1,26 @@
 package eu.kanade.tachiyomi.widget
 
 import android.content.Context
+import android.text.InputType
 import android.util.AttributeSet
+import android.widget.EditText
 import android.widget.NumberPicker
+import androidx.core.view.doOnLayout
 import eu.kanade.tachiyomi.R
+import eu.kanade.tachiyomi.util.view.findDescendant
 
 class MinMaxNumberPicker @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) :
     NumberPicker(context, attrs) {
+
+    override fun setDisplayedValues(displayedValues: Array<out String>?) {
+        super.setDisplayedValues(displayedValues)
+
+        // Disable keyboard input when a value that can't be auto-filled with number exists
+        val notNumberValue = displayedValues?.find { it.getOrNull(0)?.digitToIntOrNull() == null }
+        if (notNumberValue != null) {
+            descendantFocusability = FOCUS_BLOCK_DESCENDANTS
+        }
+    }
 
     init {
         if (attrs != null) {
@@ -17,6 +31,10 @@ class MinMaxNumberPicker @JvmOverloads constructor(context: Context, attrs: Attr
             } finally {
                 ta.recycle()
             }
+        }
+
+        doOnLayout {
+            findDescendant<EditText>()?.setRawInputType(InputType.TYPE_CLASS_NUMBER)
         }
     }
 }
