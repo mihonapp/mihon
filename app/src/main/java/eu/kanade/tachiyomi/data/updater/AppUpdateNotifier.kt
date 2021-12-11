13afa9f47673a54fb3fca112fd53ab67617bc6d9
arkon <eugcheung94@gmail.com>
2021-12-11 13:09:16 -0500

    Show version name in new update notification

--- app/src/main/java/eu/kanade/tachiyomi/data/updater/AppUpdateNotifier.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/updater/AppUpdateNotifier.kt
@@ -39,6 +39,7 @@ internal class AppUpdateNotifier(private val context: Context) {
 
         with(notificationBuilder) {
             setContentTitle(context.getString(R.string.update_check_notification_update_available))
+            setContentText(release.version)
             setSmallIcon(R.drawable.ic_download_24dp)
             setContentIntent(updateIntent)
 
