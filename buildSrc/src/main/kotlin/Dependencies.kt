0671b530bafb2161128b978a5c7785c3686fc8dd
arkon <eugcheung94@gmail.com>
2021-12-15 17:40:44 -0500

    Update to Kotlin 1.6.10

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,6 +1,6 @@
 object BuildPluginsVersion {
     const val AGP = "7.0.4"
-    const val KOTLIN = "1.6.0"
+    const val KOTLIN = "1.6.10"
     const val KOTLINTER = "3.6.0"
     const val VERSIONS_PLUGIN = "0.39.0"
     const val ABOUTLIB_PLUGIN = "8.9.3"
