8bb85753cceae158d81dcede8238d376d511f9a4
arkon <eugcheung94@gmail.com>
2021-12-31 14:40:12 -0500

    Update versions and about libraries plugins

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -2,6 +2,6 @@ object BuildPluginsVersion {
     const val AGP = "7.0.4"
     const val KOTLIN = "1.6.10"
     const val KOTLINTER = "3.6.0"
-    const val VERSIONS_PLUGIN = "0.39.0"
-    const val ABOUTLIB_PLUGIN = "8.9.3"
+    const val VERSIONS_PLUGIN = "0.40.0"
+    const val ABOUTLIB_PLUGIN = "8.9.4"
 }
