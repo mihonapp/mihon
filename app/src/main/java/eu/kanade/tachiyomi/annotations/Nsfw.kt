548e300c4b87514ff9b9c076880ef45776eacbbb
arkon <eugcheung94@gmail.com>
2022-01-01 10:51:27 -0500

    Remove unused Nsfw annotation
    Extensions now purely declare it through the Gradle config

--- app/src/main/java/eu/kanade/tachiyomi/annotations/Nsfw.kt
+++
@@ -1,6 +0,0 @@
-package eu.kanade.tachiyomi.annotations
-
-// TODO: remove this when no longer used in extensions
-@Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.CLASS)
-annotation class Nsfw
