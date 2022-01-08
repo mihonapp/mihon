544387d1a08655efcc228375aeb275f9e4492c34
arkon <eugcheung94@gmail.com>
2022-01-08 15:06:44 -0500

    Avoid reader crash

--- app/src/main/java/eu/kanade/tachiyomi/ui/reader/viewer/pager/Pager.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/reader/viewer/pager/Pager.kt
@@ -84,6 +84,8 @@ open class Pager(
     override fun onTouchEvent(ev: MotionEvent): Boolean {
         return try {
             super.onTouchEvent(ev)
+        } catch (e: NullPointerException) {
+            false
         } catch (e: IndexOutOfBoundsException) {
             false
         } catch (e: IllegalArgumentException) {
