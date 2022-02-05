7659a997cfccdcecfbb1d5a3e769a4c35a73d61a
arkon <eugcheung94@gmail.com>
2022-02-05 17:27:36 -0500

    Update versions plugin

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -2,6 +2,6 @@ object BuildPluginsVersion {
     const val AGP = "7.1.0"
     const val KOTLIN = "1.6.10"
     const val KOTLINTER = "3.6.0"
-    const val VERSIONS_PLUGIN = "0.40.0"
+    const val VERSIONS_PLUGIN = "0.42.0"
     const val ABOUTLIB_PLUGIN = "8.9.4"
 }
