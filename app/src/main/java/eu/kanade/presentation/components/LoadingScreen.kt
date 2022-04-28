78207d48ba8032103925cc13ed0f180b0a554ede
FourTOne5 <59261191+FourTOne5@users.noreply.github.com>
2022-04-28 05:54:51 -0700

    Center `CurcularProgressIndicator` in `LoadingScreen` (#7032)
    * Center `CurcularProgressIndicator` in `LoadingScreen`
    
    * Fix Import and move modifier to top

--- app/src/main/java/eu/kanade/presentation/components/LoadingScreen.kt
+++ app/src/main/java/eu/kanade/presentation/components/LoadingScreen.kt
@@ -5,12 +5,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun LoadingScreen() {
-    Box(modifier = Modifier.fillMaxSize()) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
         CircularProgressIndicator(modifier = Modifier.size(64.dp))
     }
 }
