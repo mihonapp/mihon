5fbf454652a52567d9c4e2d0bb19eb7b08b1721e
arkon <eugcheung94@gmail.com>
2022-06-12 09:59:55 -0400

    Update compose-theme-adapter

--- app/src/main/java/eu/kanade/presentation/theme/TachiyomiTheme.kt
+++ app/src/main/java/eu/kanade/presentation/theme/TachiyomiTheme.kt
@@ -3,13 +3,17 @@ package eu.kanade.presentation.theme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLayoutDirection
 import com.google.android.material.composethemeadapter3.createMdc3Theme
 
 @Composable
 fun TachiyomiTheme(content: @Composable () -> Unit) {
     val context = LocalContext.current
+    val layoutDirection = LocalLayoutDirection.current
+
     val (colorScheme, typography) = createMdc3Theme(
         context = context,
+        layoutDirection = layoutDirection,
         setTextColors = true,
     )
 
