5b8cd68cf355a15e5d5789280524d08b4c922f23
stevenyomi <95685115+stevenyomi@users.noreply.github.com>
2022-06-27 09:35:35 +0800

    RateLimitInterceptor: ignore canceled calls (#7389)
    * RateLimitInterceptor: ignore canceled calls
    
    * SpecificHostRateLimit: ignore canceled calls

--- app/src/main/java/eu/kanade/tachiyomi/network/interceptor/SpecificHostRateLimitInterceptor.kt
+++ app/src/main/java/eu/kanade/tachiyomi/network/interceptor/SpecificHostRateLimitInterceptor.kt
@@ -5,6 +5,7 @@ import okhttp3.HttpUrl
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Response
+import java.io.IOException
 import java.util.concurrent.TimeUnit
 
 /**
@@ -41,9 +42,13 @@ class SpecificHostRateLimitInterceptor(
     private val host = httpUrl.host
 
     override fun intercept(chain: Interceptor.Chain): Response {
-        if (chain.request().url.host != host) {
+        // Ignore canceled calls, otherwise they would jam the queue
+        if (chain.call().isCanceled()) {
+            throw IOException()
+        } else if (chain.request().url.host != host) {
             return chain.proceed(chain.request())
         }
+
         synchronized(requestQueue) {
             val now = SystemClock.elapsedRealtime()
             val waitTime = if (requestQueue.size < permits) {
@@ -57,6 +62,11 @@ class SpecificHostRateLimitInterceptor(
                 } else {
                     oldestReq + rateLimitMillis - now // Remaining time
                 }
+            }
+
+            // Final check
+            if (chain.call().isCanceled()) {
+                throw IOException()
             }
 
             if (requestQueue.size == permits) {
