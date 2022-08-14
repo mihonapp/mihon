2d690a09b37a55e87ce4365102ee818c5d9d1ba0
Ivan Iskandar <12537387+ivaniskandar@users.noreply.github.com>
2022-08-14 22:52:46 +0700

    Reinstate in-app language picker (#7493)
    Also add required config for android 13 system picker

--- app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsGeneralController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsGeneralController.kt
@@ -3,14 +3,20 @@ package eu.kanade.tachiyomi.ui.setting
 import android.content.Intent
 import android.os.Build
 import android.provider.Settings
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.LocaleListCompat
 import androidx.preference.PreferenceScreen
 import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.util.preference.bindTo
 import eu.kanade.tachiyomi.util.preference.defaultValue
+import eu.kanade.tachiyomi.util.preference.listPreference
+import eu.kanade.tachiyomi.util.preference.onChange
 import eu.kanade.tachiyomi.util.preference.onClick
 import eu.kanade.tachiyomi.util.preference.preference
 import eu.kanade.tachiyomi.util.preference.switchPreference
 import eu.kanade.tachiyomi.util.preference.titleRes
+import eu.kanade.tachiyomi.util.system.LocaleHelper
+import org.xmlpull.v1.XmlPullParser
 import eu.kanade.tachiyomi.data.preference.PreferenceKeys as Keys
 
 class SettingsGeneralController : SettingsController() {
@@ -37,6 +43,45 @@ class SettingsGeneralController : SettingsController() {
                     }
                     startActivity(intent)
                 }
+            }
+        }
+        listPreference {
+            key = "app_lang"
+            isPersistent = false
+            titleRes = R.string.pref_app_language
+
+            val langs = mutableListOf<Pair<String, String>>()
+
+            val parser = context.resources.getXml(R.xml.locales_config)
+            var eventType = parser.eventType
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                if (eventType == XmlPullParser.START_TAG && parser.name == "locale") {
+                    for (i in 0 until parser.attributeCount) {
+                        if (parser.getAttributeName(i) == "name") {
+                            val langTag = parser.getAttributeValue(i)
+                            langs.add(Pair(langTag, LocaleHelper.getDisplayName(langTag)))
+                        }
+                    }
+                }
+                eventType = parser.next()
+            }
+
+            langs.sortBy { it.second }
+            langs.add(0, Pair("", context.getString(R.string.label_default)))
+
+            entryValues = langs.map { it.first }.toTypedArray()
+            entries = langs.map { it.second }.toTypedArray()
+            summary = "%s"
+            value = AppCompatDelegate.getApplicationLocales().get(0)?.toLanguageTag() ?: ""
+
+            onChange { newValue ->
+                val locale = if ((newValue as String).isEmpty()) {
+                    LocaleListCompat.getEmptyLocaleList()
+                } else {
+                    LocaleListCompat.forLanguageTags(newValue)
+                }
+                AppCompatDelegate.setApplicationLocales(locale)
+                true
             }
         }
     }
