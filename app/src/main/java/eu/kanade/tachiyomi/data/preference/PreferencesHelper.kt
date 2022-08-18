1fe9b7bda7145fd56e8495eb930b78f17a15e71f
arkon <eugcheung94@gmail.com>
2022-08-18 18:54:55 -0400

    Update default user agent string

--- app/src/main/java/eu/kanade/tachiyomi/data/preference/PreferencesHelper.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/preference/PreferencesHelper.kt
@@ -294,7 +294,7 @@ class PreferencesHelper(val context: Context) {
 
     fun dohProvider() = prefs.getInt(Keys.dohProvider, -1)
 
-    fun defaultUserAgent() = flowPrefs.getString(Keys.defaultUserAgent, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.124 Safari/537.36 Edg/102.0.1245.44")
+    fun defaultUserAgent() = flowPrefs.getString(Keys.defaultUserAgent, "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0")
 
     fun lastSearchQuerySearchSettings() = flowPrefs.getString("last_search_query", "")
 
