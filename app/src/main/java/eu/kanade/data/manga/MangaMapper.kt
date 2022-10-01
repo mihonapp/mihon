5c22cbf28e86079994874b08cd4bdfb1bff2b0d9
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2022-10-01 20:33:03 +0600

    Remove mangaChapterMapper (#8124)
    Was used by updates screen previously

--- app/src/main/java/eu/kanade/data/manga/MangaMapper.kt
+++ app/src/main/java/eu/kanade/data/manga/MangaMapper.kt
@@ -1,6 +1,5 @@
 package eu.kanade.data.manga
 
-import eu.kanade.domain.chapter.model.Chapter
 import eu.kanade.domain.manga.model.Manga
 import eu.kanade.tachiyomi.data.database.models.LibraryManga
 import eu.kanade.tachiyomi.source.model.UpdateStrategy
@@ -26,43 +25,6 @@ val mangaMapper: (Long, Long, String, String?, String?, String?, List<String>?, 
             thumbnailUrl = thumbnailUrl,
             updateStrategy = updateStrategy,
             initialized = initialized,
-        )
-    }
-
-val mangaChapterMapper: (Long, Long, String, String?, String?, String?, List<String>?, String, Long, String?, Boolean, Long?, Long?, Boolean, Long, Long, Long, Long, UpdateStrategy, Long, Long, String, String, String?, Boolean, Boolean, Long, Float, Long, Long, Long) -> Pair<Manga, Chapter> =
-    { _id, source, url, artist, author, description, genre, title, status, thumbnailUrl, favorite, lastUpdate, next_update, initialized, viewerFlags, chapterFlags, coverLastModified, dateAdded, updateStrategy, chapterId, mangaId, chapterUrl, name, scanlator, read, bookmark, lastPageRead, chapterNumber, sourceOrder, dateFetch, dateUpload ->
-        Manga(
-            id = _id,
-            source = source,
-            favorite = favorite,
-            lastUpdate = lastUpdate ?: 0,
-            dateAdded = dateAdded,
-            viewerFlags = viewerFlags,
-            chapterFlags = chapterFlags,
-            coverLastModified = coverLastModified,
-            url = url,
-            title = title,
-            artist = artist,
-            author = author,
-            description = description,
-            genre = genre,
-            status = status,
-            thumbnailUrl = thumbnailUrl,
-            updateStrategy = updateStrategy,
-            initialized = initialized,
-        ) to Chapter(
-            id = chapterId,
-            mangaId = mangaId,
-            read = read,
-            bookmark = bookmark,
-            lastPageRead = lastPageRead,
-            dateFetch = dateFetch,
-            sourceOrder = sourceOrder,
-            url = chapterUrl,
-            name = name,
-            dateUpload = dateUpload,
-            chapterNumber = chapterNumber,
-            scanlator = scanlator,
         )
     }
 
