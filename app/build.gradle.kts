ebddb96373e08334c3e74b5027793308a85cb754
arkon <eugcheung94@gmail.com>
2022-10-28 11:36:26 -0400

    Release v0.14.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "eu.kanade.tachiyomi"
         minSdk = AndroidConfig.minSdk
         targetSdk = AndroidConfig.targetSdk
-        versionCode = 88
-        versionName = "0.13.6"
+        versionCode = 89
+        versionName = "0.14.0"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
