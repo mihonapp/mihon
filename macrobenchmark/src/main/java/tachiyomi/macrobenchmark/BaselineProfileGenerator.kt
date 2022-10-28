0288abb66e21110c5b144c7258014899442dd06a
arkon <eugcheung94@gmail.com>
2022-10-28 11:15:06 -0400

    Update baseline profile

--- macrobenchmark/src/main/java/tachiyomi/macrobenchmark/BaselineProfileGenerator.kt
+++ macrobenchmark/src/main/java/tachiyomi/macrobenchmark/BaselineProfileGenerator.kt
@@ -2,11 +2,13 @@ package tachiyomi.macrobenchmark
 
 import androidx.benchmark.macro.ExperimentalBaselineProfilesApi
 import androidx.benchmark.macro.junit4.BaselineProfileRule
+import androidx.test.uiautomator.By
 import org.junit.Rule
 import org.junit.Test
 
 @OptIn(ExperimentalBaselineProfilesApi::class)
 class BaselineProfileGenerator {
+
     @get:Rule
     val baselineProfileRule = BaselineProfileRule()
 
@@ -17,8 +19,13 @@ class BaselineProfileGenerator {
             pressHome()
             startActivityAndWait()
 
+            device.findObject(By.text("Updates")).click()
+            device.findObject(By.text("History")).click()
+            device.findObject(By.text("More")).click()
+
             // TODO: Navigate to browse-extensions screen when storage permission
             // in sources screen moved. Possibly open manga details screen too?
+            // device.findObject(By.text("Browse")).click()
         },
     )
 }
