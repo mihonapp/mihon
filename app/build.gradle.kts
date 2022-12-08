e748d91d4a45f6b54c1e3c7df8b859d3a08fed2b
arkon <eugcheung94@gmail.com>
2022-12-07 22:44:09 -0500

    Bump dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -187,7 +187,6 @@ dependencies {
     implementation(androidx.paging.compose)
 
     implementation(libs.bundles.sqlite)
-    implementation(androidx.sqlite)
     implementation(libs.sqldelight.android.driver)
     implementation(libs.sqldelight.coroutines)
     implementation(libs.sqldelight.android.paging)
