a14c01c1dea274ba2f71dd8f99254acbf860b957
arkon <eugcheung94@gmail.com>
2022-12-21 22:48:39 -0500

    Update baseline profile

--- macrobenchmark/src/main/java/tachiyomi/macrobenchmark/BaselineProfileGenerator.kt
+++ macrobenchmark/src/main/java/tachiyomi/macrobenchmark/BaselineProfileGenerator.kt
@@ -20,12 +20,16 @@ class BaselineProfileGenerator {
             startActivityAndWait()
 
             device.findObject(By.text("Updates")).click()
-            device.findObject(By.text("History")).click()
-            device.findObject(By.text("More")).click()
 
-            // TODO: Navigate to browse-extensions screen when storage permission
-            // in sources screen moved. Possibly open manga details screen too?
+            device.findObject(By.text("History")).click()
+
+            // TODO: automate storage permissions and possibly open manga details screen too?
             // device.findObject(By.text("Browse")).click()
+            // device.findObject(By.text("Extensions")).click()
+            // device.swipe(150, 150, 50, 150, 1)
+
+            device.findObject(By.text("More")).click()
+            device.findObject(By.text("Settings")).click()
         },
     )
 }
