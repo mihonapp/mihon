f04cf72c0cb53fdbb37daa3440482b35ea5f8591
arkon <eugcheung94@gmail.com>
2023-01-11 19:01:13 -0500

    Bump core-ktx dependency

--- app/src/main/java/eu/kanade/tachiyomi/util/system/IntentExtensions.kt
+++ app/src/main/java/eu/kanade/tachiyomi/util/system/IntentExtensions.kt
@@ -5,7 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
-import android.os.Bundle
+import androidx.core.content.IntentCompat
 import eu.kanade.tachiyomi.R
 import java.io.Serializable
 
@@ -33,12 +33,7 @@ fun Uri.toShareIntent(context: Context, type: String = "image/*", message: Strin
 }
 
 inline fun <reified T> Intent.getParcelableExtraCompat(name: String): T? {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        getParcelableExtra(name, T::class.java)
-    } else {
-        @Suppress("DEPRECATION")
-        getParcelableExtra(name)
-    }
+    return IntentCompat.getParcelableExtra(this, name, T::class.java)
 }
 
 inline fun <reified T : Serializable> Intent.getSerializableExtraCompat(name: String): T? {
@@ -47,23 +42,5 @@ inline fun <reified T : Serializable> Intent.getSerializableExtraCompat(name: St
     } else {
         @Suppress("DEPRECATION")
         getSerializableExtra(name) as? T
-    }
-}
-
-inline fun <reified T : Serializable> Bundle.getSerializableCompat(name: String): T? {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        getSerializable(name, T::class.java)
-    } else {
-        @Suppress("DEPRECATION")
-        getSerializable(name) as? T
-    }
-}
-
-inline fun <reified T> Bundle.getParcelableCompat(name: String): T? {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        getParcelable(name, T::class.java)
-    } else {
-        @Suppress("DEPRECATION")
-        getParcelable(name)
     }
 }
