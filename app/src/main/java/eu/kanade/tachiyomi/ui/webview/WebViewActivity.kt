83fda20078de449fba54ae6c31b25ce946fbfe57
arkon <eugcheung94@gmail.com>
2023-02-19 11:48:26 -0500

    Avoid crashes if headers can't be built for usage in WebView
    (cherry picked from commit ec49411bee9d7099e13685a02d54edabed863557)

--- app/src/main/java/eu/kanade/tachiyomi/ui/webview/WebViewActivity.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/webview/WebViewActivity.kt
@@ -17,6 +17,7 @@ import eu.kanade.tachiyomi.util.system.openInBrowser
 import eu.kanade.tachiyomi.util.system.toShareIntent
 import eu.kanade.tachiyomi.util.system.toast
 import eu.kanade.tachiyomi.util.view.setComposeContent
+import logcat.LogPriority
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import tachiyomi.core.util.system.logcat
 import uy.kohesive.injekt.injectLazy
@@ -47,7 +48,11 @@ class WebViewActivity : BaseActivity() {
 
         var headers = emptyMap<String, String>()
         (sourceManager.get(intent.extras!!.getLong(SOURCE_KEY)) as? HttpSource)?.let { source ->
-            headers = source.headers.toMultimap().mapValues { it.value.getOrNull(0) ?: "" }
+            try {
+                headers = source.headers.toMultimap().mapValues { it.value.getOrNull(0) ?: "" }
+            } catch (e: Exception) {
+                logcat(LogPriority.ERROR, e) { "Failed to build headers" }
+            }
         }
 
         setComposeContent {
