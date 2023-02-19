b690de55e510f920ea845f4f34c89c8a2ff419ce
arkon <eugcheung94@gmail.com>
2023-02-19 11:59:11 -0500

    Release v0.14.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
 
     defaultConfig {
         applicationId = "eu.kanade.tachiyomi"
-        versionCode = 95
-        versionName = "0.14.4"
+        versionCode = 98
+        versionName = "0.14.5"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
