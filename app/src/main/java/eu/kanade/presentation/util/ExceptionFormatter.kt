058ee4c86b15a7d4a11b9e4fb934e69a7c2ac19c
stevenyomi <95685115+stevenyomi@users.noreply.github.com>
2023-04-28 21:06:32 +0800

    Fix exception formatter's format (#9413)

--- app/src/main/java/eu/kanade/presentation/util/ExceptionFormatter.kt
+++ app/src/main/java/eu/kanade/presentation/util/ExceptionFormatter.kt
@@ -5,15 +5,17 @@ import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.network.HttpException
 import tachiyomi.data.source.NoResultsException
 import tachiyomi.domain.source.model.SourceNotInstalledException
-import java.io.IOException
 
 context(Context)
 val Throwable.formattedMessage: String
-    get() = when {
-        this is NoResultsException -> getString(R.string.no_results_found)
-        this is SourceNotInstalledException -> getString(R.string.loader_not_implemented_error)
-        this is HttpException -> "$message: ${getString(R.string.http_error_hint)}"
-        this is IOException || this is Exception -> message ?: this::class.simpleName.orEmpty()
-        this::class.simpleName != null -> "${this::class.simpleName}: $message"
-        else -> message.orEmpty()
+    get() {
+        when (this) {
+            is NoResultsException -> return getString(R.string.no_results_found)
+            is SourceNotInstalledException -> return getString(R.string.loader_not_implemented_error)
+            is HttpException -> return "$message: ${getString(R.string.http_error_hint)}"
+        }
+        return when (val className = this::class.simpleName) {
+            "Exception", "IOException" -> message ?: className
+            else -> "$className: $message"
+        }
     }
