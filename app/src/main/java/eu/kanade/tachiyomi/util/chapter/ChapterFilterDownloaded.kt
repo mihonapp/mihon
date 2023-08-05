39c6bcccd8e4e687c0864b212437a387e69cf2f3
arkon <eugcheung94@gmail.com>
2023-08-05 12:01:17 -0400

    Consider local manga as downloaded when filtering in reader
    Fixes #9801

--- app/src/main/java/eu/kanade/tachiyomi/util/chapter/ChapterFilterDownloaded.kt
+++ app/src/main/java/eu/kanade/tachiyomi/util/chapter/ChapterFilterDownloaded.kt
@@ -3,13 +3,16 @@ package eu.kanade.tachiyomi.util.chapter
 import eu.kanade.tachiyomi.data.download.DownloadCache
 import tachiyomi.domain.chapter.model.Chapter
 import tachiyomi.domain.manga.model.Manga
+import tachiyomi.source.local.isLocal
 import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
 
 /**
- * Returns a copy of the list with not downloaded chapters removed
+ * Returns a copy of the list with not downloaded chapters removed.
  */
 fun List<Chapter>.filterDownloaded(manga: Manga): List<Chapter> {
+    if (manga.isLocal()) return this
+
     val downloadCache: DownloadCache = Injekt.get()
 
     return filter { downloadCache.isChapterDownloaded(it.name, it.scanlator, manga.title, manga.source, false) }
