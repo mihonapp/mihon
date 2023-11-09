402e2c47fb549f98392b7155a6beed5a53b73f97
arkon <eugcheung94@gmail.com>
2023-11-08 22:08:19 -0500

    Fix EmptyScreen kaomoji looking broken for RTL locales

--- presentation-core/src/main/java/tachiyomi/presentation/core/screens/EmptyScreen.kt
+++ presentation-core/src/main/java/tachiyomi/presentation/core/screens/EmptyScreen.kt
@@ -12,12 +12,15 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.fastForEach
 import tachiyomi.presentation.core.components.ActionButton
@@ -59,11 +62,13 @@ fun EmptyScreen(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
-        Text(
-            text = face,
-            modifier = Modifier.secondaryItemAlpha(),
-            style = MaterialTheme.typography.displayMedium,
-        )
+        CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) {
+            Text(
+                text = face,
+                modifier = Modifier.secondaryItemAlpha(),
+                style = MaterialTheme.typography.displayMedium,
+            )
+        }
 
         Text(
             text = message,
