f20980b4c9c455703c80b120a39063e1e9a3df0a
arkon <eugcheung94@gmail.com>
2023-12-17 10:02:41 -0500

    Bump NDK
    Just using the same version as J2K for now, we can probably go higher though.

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -2,5 +2,5 @@ object AndroidConfig {
     const val compileSdk = 34
     const val minSdk = 23
     const val targetSdk = 34
-    const val ndk = "22.1.7171670"
+    const val ndk = "23.1.7779620"
 }
