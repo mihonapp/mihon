c62cd6e997cb426ce8875a5f43ebeaa183bca7a5
arkon <eugcheung94@gmail.com>
2023-12-17 11:33:08 -0500

    Bump to latest NDK LTS

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -2,5 +2,5 @@ object AndroidConfig {
     const val compileSdk = 34
     const val minSdk = 23
     const val targetSdk = 34
-    const val ndk = "23.1.7779620"
+    const val ndk = "26.1.10909125"
 }
