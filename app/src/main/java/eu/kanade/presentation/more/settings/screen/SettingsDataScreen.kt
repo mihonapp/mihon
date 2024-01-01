83130f9bf964dfce512c77803f8b5bcfb90c8f08
arkon <eugcheung94@gmail.com>
2023-12-31 23:33:10 -0500

    Try to show actual path in invalid location downloader notification
    Instead of the class/hashCode, which doesn't mean much to a user.

--- app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsDataScreen.kt
+++ app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsDataScreen.kt
@@ -42,6 +42,7 @@ import kotlinx.collections.immutable.persistentListOf
 import kotlinx.collections.immutable.persistentMapOf
 import logcat.LogPriority
 import tachiyomi.core.i18n.stringResource
+import tachiyomi.core.storage.displayablePath
 import tachiyomi.core.util.lang.launchNonCancellable
 import tachiyomi.core.util.lang.withUIContext
 import tachiyomi.core.util.system.logcat
@@ -111,7 +112,7 @@ object SettingsDataScreen : SearchableSettings {
 
         return remember(storageDir) {
             val file = UniFile.fromUri(context, storageDir.toUri())
-            file?.filePath ?: file?.uri?.toString()
+            file?.displayablePath
         } ?: stringResource(MR.strings.invalid_location, storageDir)
     }
 
