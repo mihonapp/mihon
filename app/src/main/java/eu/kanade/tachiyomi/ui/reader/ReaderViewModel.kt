5854ad97e0dbada0b75feda268bf4d57a7161d01
arkon <arkon@users.noreply.github.com>
2024-01-07 17:30:53 -0500

    Do proper check for next chapter's download status when downloading ahead
    Fixes #10151 (I think?)

--- app/src/main/java/eu/kanade/tachiyomi/ui/reader/ReaderViewModel.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/reader/ReaderViewModel.kt
@@ -462,7 +462,8 @@ class ReaderViewModel @JvmOverloads constructor(
                 manga.title,
                 manga.source,
             )
-            if (isNextChapterDownloaded) return@launchIO
+            if (!isNextChapterDownloaded) return@launchIO
+
             val chaptersToDownload = getNextChapters.await(manga.id, nextChapter.id!!).run {
                 if (readerPreferences.skipDupe().get()) {
                     removeDuplicates(nextChapter.toDomainChapter()!!)
