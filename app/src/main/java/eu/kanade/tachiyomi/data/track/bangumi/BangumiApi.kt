8c910f2a2c27dc339a4b05f03be3386a3afa2194
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2024-01-15 23:01:20 +0600

    Make tracker use Mihon's client

--- app/src/main/java/eu/kanade/tachiyomi/data/track/bangumi/BangumiApi.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/track/bangumi/BangumiApi.kt
@@ -189,7 +189,7 @@ class BangumiApi(
         private const val oauthUrl = "https://bgm.tv/oauth/access_token"
         private const val loginUrl = "https://bgm.tv/oauth/authorize"
 
-        private const val redirectUrl = "tachiyomi://bangumi-auth"
+        private const val redirectUrl = "mihon://bangumi-auth"
 
         fun authUrl(): Uri =
             loginUrl.toUri().buildUpon()
