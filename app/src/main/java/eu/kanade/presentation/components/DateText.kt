aeef9a2d4d1b65601340f01b5dcca8007c83ceac
Matthew Witman <mnwranger@gmail.com>
2024-02-15 01:53:40 -0500

    Cleaned up date util classes

--- app/src/main/java/eu/kanade/presentation/components/DateText.kt
+++ app/src/main/java/eu/kanade/presentation/components/DateText.kt
@@ -5,42 +5,22 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 import eu.kanade.domain.ui.UiPreferences
 import eu.kanade.tachiyomi.util.lang.toRelativeSting
-import eu.kanade.tachiyomi.util.lang.toRelativeString
 import tachiyomi.i18n.MR
 import tachiyomi.presentation.core.i18n.stringResource
 import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
+import java.time.Instant
 import java.time.LocalDate
-import java.util.Date
+import java.time.ZoneId
 
 @Composable
 fun relativeDateText(
     dateEpochMillis: Long,
 ): String {
     return relativeDateText(
-        date = Date(dateEpochMillis).takeIf { dateEpochMillis > 0L },
+        localDate = LocalDate.ofInstant(Instant.ofEpochMilli(dateEpochMillis), ZoneId.systemDefault()).takeIf { dateEpochMillis > 0L },
     )
 }
-
-@Composable
-fun relativeDateText(
-    date: Date?,
-): String {
-    val context = LocalContext.current
-
-    val preferences = remember { Injekt.get<UiPreferences>() }
-    val relativeTime = remember { preferences.relativeTime().get() }
-    val dateFormat = remember { UiPreferences.dateFormat(preferences.dateFormat().get()) }
-
-    return date
-        ?.toRelativeString(
-            context = context,
-            relative = relativeTime,
-            dateFormat = dateFormat,
-        )
-        ?: stringResource(MR.strings.not_applicable)
-}
-
 
 @Composable
 fun relativeDateText(
@@ -50,11 +30,12 @@ fun relativeDateText(
 
     val preferences = remember { Injekt.get<UiPreferences>() }
     val relativeTime = remember { preferences.relativeTime().get() }
-//    val dateFormat = remember { UiPreferences.dateFormat(preferences.dateFormat().get()) }
+    val dateFormat = remember { UiPreferences.dateFormat(preferences.dateFormat().get()) }
 
     return localDate?.toRelativeSting(
             context = context,
             relative = relativeTime,
+            dateFormat = dateFormat
         )
         ?: stringResource(MR.strings.not_applicable)
 }
--- app/src/main/java/eu/kanade/tachiyomi/util/lang/DateExtensions.kt
+++ app/src/main/java/eu/kanade/tachiyomi/util/lang/DateExtensions.kt
@@ -10,13 +10,13 @@ import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 import java.time.temporal.ChronoUnit
-import java.util.Calendar
 import java.util.Date
 
-fun Date.toDateTimestampString(dateFormatter: DateFormat): String {
-    val date = dateFormatter.format(this)
-    val time = DateFormat.getTimeInstance(DateFormat.SHORT).format(this)
+fun LocalDateTime.toDateTimestampString(dateTimeFormatter: DateTimeFormatter): String {
+    val date = dateTimeFormatter.format(this)
+    val time = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT).format(this)
     return "$date $time"
 }
 
@@ -39,21 +39,10 @@ fun Long.toLocalDate(): LocalDate {
     return LocalDate.ofInstant(Instant.ofEpochMilli(this), ZoneId.systemDefault());
 }
 
-/**
- * Get date as time key
- *
- * @param date desired date
- * @return date as time key
- */
-fun Long.toDateKey(): Date {
-    val instant = Instant.ofEpochMilli(this)
-    return Date.from(instant.truncatedTo(ChronoUnit.DAYS))
-}
-
 fun LocalDate.toRelativeSting(
     context: Context,
     relative: Boolean = true,
-    dateFormat: DateTimeFormatter = DateTimeFormatter.ISO_DATE,
+    dateFormat: DateTimeFormatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT),
 ): String {
     if (!relative) {
         return dateFormat.format(this)
@@ -72,41 +61,3 @@ fun LocalDate.toRelativeSting(
     }
 }
 
-fun Date.toRelativeString(
-    context: Context,
-    relative: Boolean = true,
-    dateFormat: DateFormat = DateFormat.getDateInstance(DateFormat.SHORT),
-): String {
-    if (!relative) {
-        return dateFormat.format(this)
-    }
-    val now = Date()
-    val difference = now.timeWithOffset.floorNearest(MILLISECONDS_IN_DAY) -
-        this.timeWithOffset.floorNearest(MILLISECONDS_IN_DAY)
-    val days = difference.floorDiv(MILLISECONDS_IN_DAY).toInt()
-    return when {
-        difference < 0 -> dateFormat.format(this)
-        difference < MILLISECONDS_IN_DAY -> context.stringResource(MR.strings.relative_time_today)
-        difference < MILLISECONDS_IN_DAY.times(7) -> context.pluralStringResource(
-            MR.plurals.relative_time,
-            days,
-            days,
-        )
-        else -> dateFormat.format(this)
-    }
-}
-
-private const val MILLISECONDS_IN_DAY = 86_400_000L
-
-private val Date.timeWithOffset: Long
-    get() {
-        return Calendar.getInstance().run {
-            time = this@timeWithOffset
-            val dstOffset = get(Calendar.DST_OFFSET)
-            this@timeWithOffset.time + timeZone.rawOffset + dstOffset
-        }
-    }
-
-private fun Long.floorNearest(to: Long): Long {
-    return this.floorDiv(to) * to
-}
