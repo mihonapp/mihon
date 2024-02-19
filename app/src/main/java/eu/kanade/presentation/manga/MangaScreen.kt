d374a5242bc1dd6273dcc3a9240f4ce942d25a5e
imkunet <kunettyi@gmail.com>
2024-02-19 04:25:09 -0500

    Added tablet notes

--- app/src/main/java/eu/kanade/presentation/manga/MangaScreen.kt
+++ app/src/main/java/eu/kanade/presentation/manga/MangaScreen.kt
@@ -162,7 +162,7 @@ fun MangaScreen(
             onEditCategoryClicked = onEditCategoryClicked,
             onEditIntervalClicked = onEditFetchIntervalClicked,
             onMigrateClicked = onMigrateClicked,
-            onClickNotes = onNotesClicked,
+            onNotesClicked = onNotesClicked,
             onMultiBookmarkClicked = onMultiBookmarkClicked,
             onMultiMarkAsReadClicked = onMultiMarkAsReadClicked,
             onMarkPreviousAsReadClicked = onMarkPreviousAsReadClicked,
@@ -198,7 +198,7 @@ fun MangaScreen(
             onEditCategoryClicked = onEditCategoryClicked,
             onEditIntervalClicked = onEditFetchIntervalClicked,
             onMigrateClicked = onMigrateClicked,
-            onClickNotes = onNotesClicked,
+            onNotesClicked = onNotesClicked,
             onMultiBookmarkClicked = onMultiBookmarkClicked,
             onMultiMarkAsReadClicked = onMultiMarkAsReadClicked,
             onMarkPreviousAsReadClicked = onMarkPreviousAsReadClicked,
@@ -244,7 +244,7 @@ private fun MangaScreenSmallImpl(
     onEditCategoryClicked: (() -> Unit)?,
     onEditIntervalClicked: (() -> Unit)?,
     onMigrateClicked: (() -> Unit)?,
-    onClickNotes: () -> Unit,
+    onNotesClicked: () -> Unit,
 
     // For bottom action menu
     onMultiBookmarkClicked: (List<Chapter>, bookmarked: Boolean) -> Unit,
@@ -310,7 +310,7 @@ private fun MangaScreenSmallImpl(
                 onClickEditCategory = onEditCategoryClicked,
                 onClickRefresh = onRefresh,
                 onClickMigrate = onMigrateClicked,
-                onClickNotes = onClickNotes,
+                onClickNotes = onNotesClicked,
                 actionModeCounter = selectedChapterCount,
                 onSelectAll = { onAllChapterSelected(true) },
                 onInvertSelection = { onInvertSelection() },
@@ -430,7 +430,7 @@ private fun MangaScreenSmallImpl(
                         contentType = MangaScreenItem.NOTES_SECTION,
                     ) {
                         MangaNotesSection(
-                            onClickNotes = onClickNotes,
+                            onClickNotes = onNotesClicked,
                             content = state.manga.notes,
                         )
                     }
@@ -500,7 +500,7 @@ fun MangaScreenLargeImpl(
     onEditCategoryClicked: (() -> Unit)?,
     onEditIntervalClicked: (() -> Unit)?,
     onMigrateClicked: (() -> Unit)?,
-    onClickNotes: () -> Unit,
+    onNotesClicked: () -> Unit,
 
     // For bottom action menu
     onMultiBookmarkClicked: (List<Chapter>, bookmarked: Boolean) -> Unit,
@@ -559,7 +559,7 @@ fun MangaScreenLargeImpl(
                 onClickEditCategory = onEditCategoryClicked,
                 onClickRefresh = onRefresh,
                 onClickMigrate = onMigrateClicked,
-                onClickNotes = onClickNotes,
+                onClickNotes = onNotesClicked,
                 actionModeCounter = selectedChapterCount,
                 onSelectAll = { onAllChapterSelected(true) },
                 onInvertSelection = { onInvertSelection() },
@@ -660,6 +660,10 @@ fun MangaScreenLargeImpl(
                             tagsProvider = { state.manga.genre },
                             onTagSearch = onTagSearch,
                             onCopyTagToClipboard = onCopyTagToClipboard,
+                        )
+                        MangaNotesSection(
+                            onClickNotes = onNotesClicked,
+                            content = state.manga.notes,
                         )
                     }
                 },
