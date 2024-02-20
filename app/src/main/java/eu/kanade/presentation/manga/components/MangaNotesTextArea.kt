378ad99a2370f8a1a4736a33c1d79898617375c2
imkunet <kunettyi@gmail.com>
2024-02-20 02:43:16 -0500

    Impose note editor length limit

--- app/src/main/java/eu/kanade/presentation/manga/components/MangaNotesTextArea.kt
+++ app/src/main/java/eu/kanade/presentation/manga/components/MangaNotesTextArea.kt
@@ -1,8 +1,12 @@
 package eu.kanade.presentation.manga.components
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
@@ -11,11 +15,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.TextFieldValue
 import eu.kanade.tachiyomi.ui.manga.notes.MangaNotesScreenState
+
+private const val MAX_LENGTH = 10_000
 
 @Composable
 fun MangaNotesTextArea(
@@ -34,10 +41,28 @@ fun MangaNotesTextArea(
     ) {
         OutlinedTextField(
             value = text,
-            onValueChange = { text = it },
+            onValueChange = { if (it.text.length <= MAX_LENGTH) text = it },
             modifier = Modifier
                 .fillMaxSize()
                 .focusRequester(focusRequester),
+            supportingText = {
+                val displayWarning = text.text.length > MAX_LENGTH / 10 * 9
+                if (!displayWarning) {
+                    Text(
+                        text = "0",
+                        modifier = Modifier.alpha(0f),
+                    )
+                }
+                AnimatedVisibility(
+                    displayWarning,
+                    enter = fadeIn(),
+                    exit = fadeOut(),
+                ) {
+                    Text(
+                        text = "${text.text.length} / $MAX_LENGTH",
+                    )
+                }
+            },
         )
     }
 
