c1dd69f775e4985030ae2b077091726b2d258aa8
Shamicen <Shamicen@protonmail.com>
2024-03-11 09:24:40 +0100

    fix merge conflict

--- domain/src/main/java/tachiyomi/domain/manga/model/Manga.kt
+++ domain/src/main/java/tachiyomi/domain/manga/model/Manga.kt
@@ -123,6 +123,7 @@ data class Manga(
             updateStrategy = UpdateStrategy.ALWAYS_UPDATE,
             initialized = false,
             lastModifiedAt = 0L,
+            favoriteModifiedAt = null,
             version = 0L,
             webUrls = null,
         )
