2f549db3b0f583e4ce4d64e8e9debd8fbda987aa
Matthew Witman <mnwranger@gmail.com>
2024-03-22 13:25:55 -0400

    Fixed error introduced in cleanup

--- domain/src/main/java/mihon/domain/extensionrepo/interactor/CreateExtensionRepo.kt
+++ domain/src/main/java/mihon/domain/extensionrepo/interactor/CreateExtensionRepo.kt
@@ -43,19 +43,33 @@ class CreateExtensionRepo(
             Result.Success
         } catch (ex: SaveExtensionRepoException) {
             logcat(LogPriority.WARN, ex) { "SQL Conflict attempting to add new repository ${repo.baseUrl}" }
-            // SQLDelight doesn't provide constraint info in exceptions.
-            // First check if the conflict was on primary key. if so return RepoAlreadyExists
-            // Then check if the conflict was on fingerprint. if so Return DuplicateFingerprint
-            when {
-                extensionRepoRepository.getRepository(baseUrl = repo.baseUrl) != null -> {
-                    Result.RepoAlreadyExists
-                }
-                extensionRepoRepository.getRepositoryByFingerprint(fingerprint = repo.fingerprint) != null -> {
-                    Result.DuplicateFingerprint(it, repo) 
-                }
-                else -> Result.Error
-            }
+            return handleInsertionError(repo)
         }
+    }
+
+    @Suppress("ReturnCount")
+    // Three returns is much cleaner than let + elvis chaining here.
+    // When matching does not work because we need to capture the result from getRepositoryByFingerprint
+    /**
+     * Error Handler for insert when there are trying to create new repositories
+     *
+     *  SaveExtensionRepoException doesn't provide constraint info in exceptions.
+     *  First check if the conflict was on primary key. if so return RepoAlreadyExists
+     *  Then check if the conflict was on fingerprint. if so Return DuplicateFingerprint
+     *  If neither are found, there was some other Error, and return Result.Error
+     *
+     *  @param repo Extension Repo holder for passing to DB/Error Dialog
+     */
+    private suspend fun handleInsertionError(repo: ExtensionRepo): Result {
+        val repoExists = extensionRepoRepository.getRepository(repo.baseUrl)
+        if (repoExists != null) {
+            return Result.RepoAlreadyExists
+        }
+        val matchingFingerprintRepo = extensionRepoRepository.getRepositoryByFingerprint(repo.fingerprint)
+        if (matchingFingerprintRepo != null) {
+            return Result.DuplicateFingerprint(matchingFingerprintRepo, repo)
+        }
+        return Result.Error
     }
 
     sealed interface Result {
