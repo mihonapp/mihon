1d66565e7719edbb1d6ed0c4f1fa76caf6e19d79
Cologler <skyoflw@gmail.com>
2024-04-08 00:41:36 +0800

    fix: insert with same order

--- app/src/main/java/eu/kanade/tachiyomi/data/backup/restore/restorers/CategoriesRestorer.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/backup/restore/restorers/CategoriesRestorer.kt
@@ -17,14 +17,18 @@ class CategoriesRestorer(
         if (backupCategories.isNotEmpty()) {
             val dbCategories = getCategories.await()
             val dbCategoriesByName = dbCategories.associateBy { it.name }
+            var nextOrder = dbCategories.maxOfOrNull { it.order }?.plus(1) ?: 0
 
-            val categories = backupCategories.map {
-                dbCategoriesByName[it.name]
-                    ?: handler.awaitOneExecutable {
-                        categoriesQueries.insert(it.name, it.order, it.flags)
-                        categoriesQueries.selectLastInsertedRowId()
-                    }.let { id -> it.toCategory(id) }
-            }
+            val categories = backupCategories
+                .sortedBy { it.order }
+                .map {
+                    val newOrder = nextOrder++
+                    dbCategoriesByName[it.name]
+                        ?: handler.awaitOneExecutable {
+                            categoriesQueries.insert(it.name, newOrder, it.flags)
+                            categoriesQueries.selectLastInsertedRowId()
+                        }.let { id -> it.toCategory(id).copy(order = newOrder) }
+                }
 
             libraryPreferences.categorizedDisplaySettings().set(
                 (dbCategories + categories)
