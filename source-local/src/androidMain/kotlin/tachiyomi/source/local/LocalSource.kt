7ec2108812fbe0483111dbe996e29e5a621b583a
Radon Rosborough <radon@intuitiveexplanations.com>
2024-05-04 09:40:41 -0700

    Massively improve findFile performance (#728)
    * Massively improve findFile performance
    
    * Update libs.versions.toml
    
    ---------
    
    Co-authored-by: AntsyLich <59261191+AntsyLich@users.noreply.github.com>

--- source-local/src/androidMain/kotlin/tachiyomi/source/local/LocalSource.kt
+++ source-local/src/androidMain/kotlin/tachiyomi/source/local/LocalSource.kt
@@ -300,8 +300,8 @@ actual class LocalSource(
         try {
             val (mangaDirName, chapterName) = chapter.url.split('/', limit = 2)
             return fileSystem.getBaseDirectory()
-                ?.findFile(mangaDirName, true)
-                ?.findFile(chapterName, true)
+                ?.findFile(mangaDirName)
+                ?.findFile(chapterName)
                 ?.let(Format.Companion::valueOf)
                 ?: throw Exception(context.stringResource(MR.strings.chapter_not_found))
         } catch (e: Format.UnknownFormatException) {
