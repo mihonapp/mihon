dbcc4a7d714cf36b48a41d6074ef671f3c039823
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2024-05-05 03:31:50 +0600

    Revert "Fix badge count getting cut off on tab title"
    This reverts commit 263e467cdeb948b8f3679e2ea0282a291cf2f131.

--- presentation-core/src/main/java/tachiyomi/presentation/core/components/material/Tabs.kt
+++ presentation-core/src/main/java/tachiyomi/presentation/core/components/material/Tabs.kt
@@ -6,7 +6,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.sp
 import tachiyomi.presentation.core.components.Pill
@@ -22,7 +21,6 @@ fun TabText(text: String, badgeCount: Int? = null) {
             text = text,
             maxLines = 1,
             overflow = TextOverflow.Ellipsis,
-            modifier = Modifier.weight(1f, fill = false),
         )
         if (badgeCount != null) {
             Pill(
