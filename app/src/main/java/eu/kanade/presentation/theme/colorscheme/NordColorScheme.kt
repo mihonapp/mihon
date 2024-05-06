2465184cc14348540117fb3cb28039055f924861
CrepeTF <70870719+CrepeTF@users.noreply.github.com>
2024-05-06 22:21:40 +0100

    Update Nord theme

--- app/src/main/java/eu/kanade/presentation/theme/colorscheme/NordColorScheme.kt
+++ app/src/main/java/eu/kanade/presentation/theme/colorscheme/NordColorScheme.kt
@@ -17,19 +17,19 @@ internal object NordColorScheme : BaseColorScheme() {
         primaryContainer = Color(0xFF88C0D0),
         onPrimaryContainer = Color(0xFF2E3440),
         inversePrimary = Color(0xFF397E91),
-        secondary = Color(0xFF81A1C1),
-        onSecondary = Color(0xFF2E3440),
-        secondaryContainer = Color(0xFF81A1C1),
-        onSecondaryContainer = Color(0xFF2E3440),
-        tertiary = Color(0xFF5E81AC),
-        onTertiary = Color(0xFF000000),
+        secondary = Color(0xFF81A1C1), // Unread badge
+        onSecondary = Color(0xFF2E3440), // Unread badge text
+        secondaryContainer = Color(0xFF81A1C1), // Navigation bar selector pill
+        onSecondaryContainer = Color(0xFF2E3440), // Navigation bar selector icon
+        tertiary = Color(0xFF5E81AC), // Downloaded badge
+        onTertiary = Color(0xFF000000), // Downloaded badge text
         tertiaryContainer = Color(0xFF5E81AC),
         onTertiaryContainer = Color(0xFF000000),
         background = Color(0xFF2E3440),
         onBackground = Color(0xFFECEFF4),
-        surface = Color(0xFF3B4252),
+        surface = Color(0xFF2E3440),
         onSurface = Color(0xFFECEFF4),
-        surfaceVariant = Color(0xFF2E3440),
+        surfaceVariant = Color(0xFF414C5C), // Navigation bar background (ThemePrefWidget)
         onSurfaceVariant = Color(0xFFECEFF4),
         surfaceTint = Color(0xFF88C0D0),
         inverseSurface = Color(0xFFD8DEE9),
@@ -39,6 +39,7 @@ internal object NordColorScheme : BaseColorScheme() {
         onError = Color(0xFF2E3440),
         errorContainer = Color(0xFFBF616A),
         onErrorContainer = Color(0xFF000000),
+        surfaceContainer = Color(0xFF414C5C), // Navigation bar background
     )
 
     override val lightScheme = lightColorScheme(
@@ -47,19 +48,19 @@ internal object NordColorScheme : BaseColorScheme() {
         primaryContainer = Color(0xFF5E81AC),
         onPrimaryContainer = Color(0xFF000000),
         inversePrimary = Color(0xFF8CA8CD),
-        secondary = Color(0xFF81A1C1),
-        onSecondary = Color(0xFF2E3440),
-        secondaryContainer = Color(0xFF81A1C1),
-        onSecondaryContainer = Color(0xFF2E3440),
-        tertiary = Color(0xFF88C0D0),
-        onTertiary = Color(0xFF2E3440),
+        secondary = Color(0xFF81A1C1), // Unread badge
+        onSecondary = Color(0xFF2E3440), // Unread badge text
+        secondaryContainer = Color(0xFF81A1C1), // Navigation bar selector pill
+        onSecondaryContainer = Color(0xFF2E3440), // Navigation bar selector icon
+        tertiary = Color(0xFF88C0D0), // Downloaded badge
+        onTertiary = Color(0xFF2E3440), // Downloaded badge text
         tertiaryContainer = Color(0xFF88C0D0),
         onTertiaryContainer = Color(0xFF2E3440),
         background = Color(0xFFECEFF4),
         onBackground = Color(0xFF2E3440),
         surface = Color(0xFFE5E9F0),
         onSurface = Color(0xFF2E3440),
-        surfaceVariant = Color(0xFFffffff),
+        surfaceVariant = Color(0xFFDAE0EA), // Navigation bar background (ThemePrefWidget)
         onSurfaceVariant = Color(0xFF2E3440),
         surfaceTint = Color(0xFF5E81AC),
         inverseSurface = Color(0xFF3B4252),
@@ -68,5 +69,6 @@ internal object NordColorScheme : BaseColorScheme() {
         onError = Color(0xFFECEFF4),
         errorContainer = Color(0xFFBF616A),
         onErrorContainer = Color(0xFF000000),
+        surfaceContainer = Color(0xFFDAE0EA), // Navigation bar background
     )
 }
