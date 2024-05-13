16392adcbba4027cbed0a44e2fc62df330af6385
CrepeTF <70870719+CrepeTF@users.noreply.github.com>
2024-05-13 21:44:40 +0100

    Update themes to follow new compose update changes (#766)
    * Update Green Apple theme
    
    * Add some Green Apple theme comments
    
    * Update Lavender theme
    
    * Update Midnight Dusk theme
    
    * Update Nord theme
    
    * Update Strawberry Daiquiri theme
    
    * Update Tako theme
    
    * Update Teal & Turquoise theme
    
    * Update Lavender secondaryContainer and onSecondaryContainer colour
    
    * Update M.Dusk secondaryContainer and onSecondaryContainer colour
    
    * Update Tako secondaryContainer and onSecondaryContainer colour
    
    * Comments
    
    * Update Tidal Wave theme
    
    * Update Yin Yang theme
    
    * Update Yotsuba theme
    
    * Fix navbar tinted background on pure black
    
    * Add surfaceContainer levels to Lavender theme
    
    * Resolve detekt issues
    
    * Add surfaceContainer levels to Midnight Dusk theme
    
    * Add surfaceContainer levels to Nord theme
    
    * Add surfaceContainer levels to Tako theme
    
    * Add surfaceContainer levels to Teal & Turquoise theme
    
    * Add surfaceContainer levels to Tidal Wave theme
    
    * Add surfaceContainer levels to Yin Yang theme
    
    * Add surfaceContainer levels to Yotsuba theme
    
    * Add dark theme surfaceContainer levels to Yotsuba theme
    
    * surfaceContainer tweaks to Yotsuba theme
    
    * surfaceContainer tweaks to Strawberry Daiquiri theme
    
    * surfaceContainer tweaks to Nord theme
    
    * surfaceContainer tweaks to Lavender theme
    
    * Update Tachiyomi theme
    
    * Update Pure Black theme
    
    * Resolve detekt issues
    
    * Oopsie

--- app/src/main/java/eu/kanade/presentation/theme/colorscheme/TachiyomiColorScheme.kt
+++ app/src/main/java/eu/kanade/presentation/theme/colorscheme/TachiyomiColorScheme.kt
@@ -22,19 +22,19 @@ internal object TachiyomiColorScheme : BaseColorScheme() {
         primaryContainer = Color(0xFF00429B),
         onPrimaryContainer = Color(0xFFD9E2FF),
         inversePrimary = Color(0xFF0058CA),
-        secondary = Color(0xFFB0C6FF),
-        onSecondary = Color(0xFF002D6E),
-        secondaryContainer = Color(0xFF00429B),
-        onSecondaryContainer = Color(0xFFD9E2FF),
-        tertiary = Color(0xFF7ADC77),
-        onTertiary = Color(0xFF003909),
+        secondary = Color(0xFFB0C6FF), // Unread badge
+        onSecondary = Color(0xFF002D6E), // Unread badge text
+        secondaryContainer = Color(0xFF00429B), // Navigation bar selector pill & pro
+        onSecondaryContainer = Color(0xFFD9E2FF), // Navigation bar selector icon
+        tertiary = Color(0xFF7ADC77), // Downloaded badge
+        onTertiary = Color(0xFF003909), // Downloaded badge text
         tertiaryContainer = Color(0xFF005312),
         onTertiaryContainer = Color(0xFF95F990),
         background = Color(0xFF1B1B1F),
         onBackground = Color(0xFFE3E2E6),
         surface = Color(0xFF1B1B1F),
         onSurface = Color(0xFFE3E2E6),
-        surfaceVariant = Color(0xFF44464F),
+        surfaceVariant = Color(0xFF211F26), // Navigation bar background (ThemePrefWidget)
         onSurfaceVariant = Color(0xFFC5C6D0),
         surfaceTint = Color(0xFFB0C6FF),
         inverseSurface = Color(0xFFE3E2E6),
@@ -45,6 +45,11 @@ internal object TachiyomiColorScheme : BaseColorScheme() {
         onErrorContainer = Color(0xFFFFDAD6),
         outline = Color(0xFF8F9099),
         outlineVariant = Color(0xFF44464F),
+        surfaceContainerLowest = Color(0xFF1A181D),
+        surfaceContainerLow = Color(0xFF1E1C22),
+        surfaceContainer = Color(0xFF211F26), // Navigation bar background
+        surfaceContainerHigh = Color(0xFF292730),
+        surfaceContainerHighest = Color(0xFF302E38),
     )
 
     override val lightScheme = lightColorScheme(
@@ -53,19 +58,19 @@ internal object TachiyomiColorScheme : BaseColorScheme() {
         primaryContainer = Color(0xFFD9E2FF),
         onPrimaryContainer = Color(0xFF001945),
         inversePrimary = Color(0xFFB0C6FF),
-        secondary = Color(0xFF0058CA),
-        onSecondary = Color(0xFFFFFFFF),
-        secondaryContainer = Color(0xFFD9E2FF),
-        onSecondaryContainer = Color(0xFF001945),
-        tertiary = Color(0xFF006E1B),
-        onTertiary = Color(0xFFFFFFFF),
+        secondary = Color(0xFF0058CA), // Unread badge
+        onSecondary = Color(0xFFFFFFFF), // Unread badge text
+        secondaryContainer = Color(0xFFD9E2FF), // Navigation bar selector pill & progress indicator (remaining)
+        onSecondaryContainer = Color(0xFF001945), // Navigation bar selector icon
+        tertiary = Color(0xFF006E1B), // Downloaded badge
+        onTertiary = Color(0xFFFFFFFF), // Downloaded badge text
         tertiaryContainer = Color(0xFF95F990),
         onTertiaryContainer = Color(0xFF002203),
         background = Color(0xFFFEFBFF),
         onBackground = Color(0xFF1B1B1F),
         surface = Color(0xFFFEFBFF),
         onSurface = Color(0xFF1B1B1F),
-        surfaceVariant = Color(0xFFE1E2EC),
+        surfaceVariant = Color(0xFFF3EDF7), // Navigation bar background (ThemePrefWidget)
         onSurfaceVariant = Color(0xFF44464F),
         surfaceTint = Color(0xFF0058CA),
         inverseSurface = Color(0xFF303034),
@@ -76,5 +81,10 @@ internal object TachiyomiColorScheme : BaseColorScheme() {
         onErrorContainer = Color(0xFF410002),
         outline = Color(0xFF757780),
         outlineVariant = Color(0xFFC5C6D0),
+        surfaceContainerLowest = Color(0xFFF5F1F8),
+        surfaceContainerLow = Color(0xFFF7F2FA),
+        surfaceContainer = Color(0xFFF3EDF7), // Navigation bar background
+        surfaceContainerHigh = Color(0xFFFCF7FF),
+        surfaceContainerHighest = Color(0xFFFCF7FF),
     )
 }
