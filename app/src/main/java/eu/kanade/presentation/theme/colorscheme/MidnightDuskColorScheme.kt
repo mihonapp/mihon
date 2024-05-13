7b402a925a55842316c8817008a6f6bdc4e9da6d
CrepeTF <70870719+CrepeTF@users.noreply.github.com>
2024-05-13 18:54:35 +0100

    Add surfaceContainer levels to Midnight Dusk theme

--- app/src/main/java/eu/kanade/presentation/theme/colorscheme/MidnightDuskColorScheme.kt
+++ app/src/main/java/eu/kanade/presentation/theme/colorscheme/MidnightDuskColorScheme.kt
@@ -25,7 +25,7 @@ internal object MidnightDuskColorScheme : BaseColorScheme() {
         inversePrimary = Color(0xFFF02475),
         secondary = Color(0xFFF02475), // Unread badge
         onSecondary = Color(0xFF16151D), // Unread badge text
-        secondaryContainer = Color(0xFF801D4A), // Navigation bar selector pill & progress indicator (remaining)
+        secondaryContainer = Color(0xFF66183C), // Navigation bar selector pill & progress indicator (remaining)
         onSecondaryContainer = Color(0xFFF02475), // Navigation bar selector icon
         tertiary = Color(0xFF55971C), // Downloaded badge
         onTertiary = Color(0xFF16151D), // Downloaded badge text
@@ -35,13 +35,17 @@ internal object MidnightDuskColorScheme : BaseColorScheme() {
         onBackground = Color(0xFFE5E1E5),
         surface = Color(0xFF16151D),
         onSurface = Color(0xFFE5E1E5),
-        surfaceVariant = Color(0xFF591A38), // Navigation bar background (ThemePrefWidget)
+        surfaceVariant = Color(0xFF281624), // Navigation bar background (ThemePrefWidget)
         onSurfaceVariant = Color(0xFFD6C1C4),
         surfaceTint = Color(0xFFF02475),
         inverseSurface = Color(0xFF333043),
         inverseOnSurface = Color(0xFFFFFFFF),
         outline = Color(0xFF9F8C8F),
+        surfaceContainerLowest = Color(0xFF221320),
+        surfaceContainerLow = Color(0xFF251522),
         surfaceContainer = Color(0xFF281624), // Navigation bar background
+        surfaceContainerHigh = Color(0xFF2D1C2A),
+        surfaceContainerHighest = Color(0xFF2F1F2C),
     )
 
     override val lightScheme = lightColorScheme(
@@ -68,6 +72,10 @@ internal object MidnightDuskColorScheme : BaseColorScheme() {
         inverseSurface = Color(0xFF313033),
         inverseOnSurface = Color(0xFFF4F0F4),
         outline = Color(0xFF847376),
+        surfaceContainerLowest = Color(0xFFDAC0CD),
+        surfaceContainerLow = Color(0xFFE8D1DD),
         surfaceContainer = Color(0xFFF9E6F1), // Navigation bar background
+        surfaceContainerHigh = Color(0xFFFCF3F8),
+        surfaceContainerHighest = Color(0xFFFEF9FC),
     )
 }
