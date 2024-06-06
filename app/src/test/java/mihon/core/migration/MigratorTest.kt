da62c7a21a81f513988fa64df6253376f85228ef
Cuong M. Tran <cuongtran.tm@gmail.com>
2024-06-07 04:35:26 +0700

    Fix MigratorTest after update to io.mockk v1.13.11 (#814)
    * Fix MigratorTest after update to io.mockk v1.13.11
    
    Causing error: io.mockk.MockKException: was not can only be called on a mocked object
    
    * remove import

--- app/src/test/java/mihon/core/migration/MigratorTest.kt
+++ app/src/test/java/mihon/core/migration/MigratorTest.kt
@@ -1,6 +1,5 @@
 package mihon.core.migration
 
-import io.mockk.Called
 import io.mockk.slot
 import io.mockk.spyk
 import io.mockk.verify
@@ -59,7 +58,7 @@ class MigratorTest {
         val result = execute.await()
         assertFalse(result)
 
-        verify { migrationJobFactory.create(any()) wasNot Called }
+        verify(exactly = 0) { migrationJobFactory.create(any()) }
     }
 
     @Test
@@ -72,7 +71,7 @@ class MigratorTest {
         val result = execute.await()
         assertFalse(result)
 
-        verify { migrationJobFactory.create(any()) wasNot Called }
+        verify(exactly = 0) { migrationJobFactory.create(any()) }
     }
 
     @Test
