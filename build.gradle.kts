efaf28589ee4e100fdaa222b938be6edf2aeb7fb
FooIbar <118464521+FooIbar@users.noreply.github.com>
2024-06-16 09:44:08 +0000

    Fix R8 version configuration not working
    This reverts commit f3226fb278cab87422255e04e647c50095b61529.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,5 @@
 buildscript {
-    // https://issuetracker.google.com/344363457
-    // TODO: Remove when AGP's bundled R8 is updated
-    repositories {
-        maven("https://storage.googleapis.com/r8-releases/raw")
-    }
     dependencies {
-        classpath("com.android.tools:r8:8.5.21")
         classpath(libs.android.shortcut.gradle)
         classpath(libs.google.services.gradle)
         classpath(libs.aboutLibraries.gradle)
