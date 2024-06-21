7053bccec4a92f63fe941b54746ece32b1a2f159
FooIbar <118464521+FooIbar@users.noreply.github.com>
2024-06-21 23:54:54 +0800

    Fix unexpected skips in strong skipping mode

--- source-api/build.gradle.kts
+++ source-api/build.gradle.kts
@@ -13,6 +13,9 @@ kotlin {
                 api(libs.injekt.core)
                 api(libs.rxjava)
                 api(libs.jsoup)
+
+                implementation(project.dependencies.platform(compose.bom))
+                implementation(compose.runtime)
             }
         }
         val androidMain by getting {
