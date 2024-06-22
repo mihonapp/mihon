172a0d8bea2f439b610476dd3b2163f02ba9a912
Maddie Witman <mnwranger@gmail.com>
2024-06-22 19:40:52 -0400

    Recommit for e-ink pref changes

--- app/src/main/java/eu/kanade/presentation/reader/DisplayRefreshHost.kt
+++ app/src/main/java/eu/kanade/presentation/reader/DisplayRefreshHost.kt
@@ -7,19 +7,35 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import eu.kanade.tachiyomi.ui.reader.setting.ReaderPreferences
 import kotlinx.coroutines.delay
-import kotlin.time.Duration.Companion.seconds
+import kotlinx.coroutines.launch
+import tachiyomi.presentation.core.util.collectAsState
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
+import kotlin.time.Duration.Companion.milliseconds
 
 @Stable
 class DisplayRefreshHost {
 
     internal var currentDisplayRefresh by mutableStateOf(false)
+    private val readerPreferences = Injekt.get<ReaderPreferences>()
+
+    internal val flashMillis = readerPreferences.flashDurationMillis()
+    internal val flashMode = readerPreferences.flashColor()
+
+    private val flashInterval = readerPreferences.flashPageInterval().get()
+    private var timesCalled = 0
 
     fun flash() {
-        currentDisplayRefresh = true
+        if (timesCalled % flashInterval == 0) {
+            currentDisplayRefresh = true
+        }
+        timesCalled += 1
     }
 }
 
@@ -29,10 +45,24 @@ fun DisplayRefreshHost(
     modifier: Modifier = Modifier,
 ) {
     val currentDisplayRefresh = hostState.currentDisplayRefresh
+    val refreshDuration by hostState.flashMillis.collectAsState()
+    val flashMode by hostState.flashMode.collectAsState()
+
+    var bothColor by remember { mutableStateOf(Color.White) }
+
     LaunchedEffect(currentDisplayRefresh) {
         if (currentDisplayRefresh) {
-            delay(1.5.seconds)
-            hostState.currentDisplayRefresh = false
+            launch {
+                delay(refreshDuration.milliseconds)
+                hostState.currentDisplayRefresh = false
+            }
+            if (flashMode == ReaderPreferences.FlashColor.WHITE_BLACK) {
+                launch {
+                    bothColor = Color.White
+                    delay(refreshDuration.milliseconds / 2)
+                    bothColor = Color.Black
+                }
+            }
         }
     }
 
@@ -40,7 +70,11 @@ fun DisplayRefreshHost(
         modifier = modifier.fillMaxSize(),
     ) {
         if (currentDisplayRefresh) {
-            drawRect(Color.Black)
+            when (flashMode) {
+                ReaderPreferences.FlashColor.BLACK -> drawRect(Color.Black)
+                ReaderPreferences.FlashColor.WHITE -> drawRect(Color.White)
+                ReaderPreferences.FlashColor.WHITE_BLACK -> drawRect(bothColor)
+            }
         }
     }
 }
