0ce1cf22cdbb7d82df3db1a901253b4973ab027f
FooIbar <118464521+FooIbar@users.noreply.github.com>
2024-06-23 05:53:49 +0800

    Fix unexpected skips in strong skipping mode (#940)

--- source-api/src/commonMain/kotlin/eu/kanade/tachiyomi/source/model/FilterList.kt
+++ source-api/src/commonMain/kotlin/eu/kanade/tachiyomi/source/model/FilterList.kt
@@ -1,5 +1,8 @@
 package eu.kanade.tachiyomi.source.model
 
+import androidx.compose.runtime.Stable
+
+@Stable
 data class FilterList(val list: List<Filter<*>>) : List<Filter<*>> by list {
 
     constructor(vararg fs: Filter<*>) : this(if (fs.isNotEmpty()) fs.asList() else emptyList())
