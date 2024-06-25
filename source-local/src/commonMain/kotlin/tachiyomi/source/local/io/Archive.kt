bf936a844127f378003a62d1cbefac378265fb59
FooIbar <118464521+FooIbar@users.noreply.github.com>
2024-06-25 14:20:40 +0000

    Fixup

--- source-local/src/commonMain/kotlin/tachiyomi/source/local/io/Archive.kt
+++ source-local/src/commonMain/kotlin/tachiyomi/source/local/io/Archive.kt
@@ -1,15 +1,13 @@
 package tachiyomi.source.local.io
 
 import com.hippo.unifile.UniFile
+import tachiyomi.core.common.storage.extension
 
 object Archive {
 
     private val SUPPORTED_ARCHIVE_TYPES = listOf("zip", "cbz", "rar", "cbr", "7z", "cb7", "tar", "cbt", "epub")
 
     fun isSupported(file: UniFile): Boolean {
-        val name = file.name?.lowercase() ?: return false
-        val extension = name.substringAfterLast('.')
-        val nameWithoutExtension = name.substringBeforeLast('.')
-        return extension in SUPPORTED_ARCHIVE_TYPES
+        return file.extension?.lowercase() in SUPPORTED_ARCHIVE_TYPES
     }
 }
