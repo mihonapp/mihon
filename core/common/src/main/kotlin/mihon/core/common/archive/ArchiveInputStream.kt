e620665dda9eb5cc39f09e6087ea4f60a3cbe150
Ahmad Ansori Palembani <46041660+null2264@users.noreply.github.com>
2024-06-30 20:57:29 +0700

    Add safeguard to prevent ArchiveInputStream from being closed twice (#967)
    * fix: Add safeguard to prevent ArchiveInputStream from being closed twice
    
    * detekt
    
    * lint: Make detekt happy
    
    ---------
    
    Co-authored-by: AntsyLich <59261191+AntsyLich@users.noreply.github.com>

--- core/common/src/main/kotlin/mihon/core/common/archive/ArchiveInputStream.kt
+++ core/common/src/main/kotlin/mihon/core/common/archive/ArchiveInputStream.kt
@@ -5,8 +5,14 @@ import me.zhanghai.android.libarchive.ArchiveEntry
 import me.zhanghai.android.libarchive.ArchiveException
 import java.io.InputStream
 import java.nio.ByteBuffer
+import kotlin.concurrent.Volatile
 
 class ArchiveInputStream(buffer: Long, size: Long) : InputStream() {
+    private val lock = Any()
+
+    @Volatile
+    private var isClosed = false
+
     private val archive = Archive.readNew()
 
     init {
@@ -41,6 +47,11 @@ class ArchiveInputStream(buffer: Long, size: Long) : InputStream() {
     }
 
     override fun close() {
+        synchronized(lock) {
+            if (isClosed) return
+            isClosed = true
+        }
+
         Archive.readFree(archive)
     }
 
