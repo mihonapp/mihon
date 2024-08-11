78bd52e80ab036d79fd3b19f4c118aea16f85f74
MajorTanya <github@majortanya.eu>
2024-08-12 01:24:25 +0200

    Fix some migrations never running
    Both `SetupBackupCreateMigration` and `SetupLibraryUpdateMigration` were
    trying to get the `App` class from Injekt which is never provided via
    the `AppModule`. Using `Application` instead works since the
    `workManager` property used by the respective `setupTask` functions is
    an extension property on `Context`.
    
    Co-authored-by: AntsyLich <59261191+AntsyLich@users.noreply.github.com>

--- app/src/main/java/mihon/core/migration/migrations/SetupLibraryUpdateMigration.kt
+++ app/src/main/java/mihon/core/migration/migrations/SetupLibraryUpdateMigration.kt
@@ -1,6 +1,6 @@
 package mihon.core.migration.migrations
 
-import eu.kanade.tachiyomi.App
+import android.app.Application
 import eu.kanade.tachiyomi.data.library.LibraryUpdateJob
 import mihon.core.migration.Migration
 import mihon.core.migration.MigrationContext
@@ -9,7 +9,7 @@ class SetupLibraryUpdateMigration : Migration {
     override val version: Float = Migration.ALWAYS
 
     override suspend fun invoke(migrationContext: MigrationContext): Boolean {
-        val context = migrationContext.get<App>() ?: return false
+        val context = migrationContext.get<Application>() ?: return false
         LibraryUpdateJob.setupTask(context)
         return true
     }
