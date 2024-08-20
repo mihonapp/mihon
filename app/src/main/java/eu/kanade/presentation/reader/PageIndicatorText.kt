3d703421e24f7494138899a4365138f3930af612
Ruliam dos Santos de Oliveira <ruliam.oliveira@gmail.com>
2024-08-20 09:46:41 -0300

    feat: Adding bottom padding to Page Indicator to avoid phone's frame hide it

--- app/src/main/java/eu/kanade/presentation/reader/PageIndicatorText.kt
+++ app/src/main/java/eu/kanade/presentation/reader/PageIndicatorText.kt
@@ -1,16 +1,19 @@
 package eu.kanade.presentation.reader
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import eu.kanade.presentation.theme.TachiyomiPreviewTheme
 
@@ -35,6 +38,7 @@ fun PageIndicatorText(
     )
 
     Box(
+        modifier = Modifier.padding(bottom = 12.dp),
         contentAlignment = Alignment.Center,
     ) {
         Text(
