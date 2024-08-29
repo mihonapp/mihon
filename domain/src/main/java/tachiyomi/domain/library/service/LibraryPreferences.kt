dcc1c5d572685545183c1df29bf1ecb215704082
FlaminSarge <flaminsarge@gmail.com>
2024-08-29 02:54:44 -0700

    Add advanced Library setting to allow library manga's titles to update from source

--- domain/src/main/java/tachiyomi/domain/library/service/LibraryPreferences.kt
+++ domain/src/main/java/tachiyomi/domain/library/service/LibraryPreferences.kt
@@ -189,6 +189,8 @@ class LibraryPreferences(
         ChapterSwipeAction.ToggleRead,
     )
 
+    fun updateMangaTitles() = preferenceStore.getBoolean("pref_update_library_manga_titles", false)
+
     // endregion
 
     enum class ChapterSwipeAction {
