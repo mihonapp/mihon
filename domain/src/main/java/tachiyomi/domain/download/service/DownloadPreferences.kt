9c1905ede750f0229fad1a01431058b1cc9fb32d
Smol Ame <155411819+Smol-Ame@users.noreply.github.com>
2024-09-02 12:46:28 -0700

    Enable 'Split Tall Images' by default (#1185)

--- domain/src/main/java/tachiyomi/domain/download/service/DownloadPreferences.kt
+++ domain/src/main/java/tachiyomi/domain/download/service/DownloadPreferences.kt
@@ -13,7 +13,7 @@ class DownloadPreferences(
 
     fun saveChaptersAsCBZ() = preferenceStore.getBoolean("save_chapter_as_cbz", true)
 
-    fun splitTallImages() = preferenceStore.getBoolean("split_tall_images", false)
+    fun splitTallImages() = preferenceStore.getBoolean("split_tall_images", true)
 
     fun autoDownloadWhileReading() = preferenceStore.getInt("auto_download_while_reading", 0)
 
