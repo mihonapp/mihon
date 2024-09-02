3b7e0db5c7bdbd773778d5f29f0b5a13d71f4830
ɴᴇᴋᴏ <s99095lkjjim@gmail.com>
2024-09-02 12:54:42 +0800

    show name if name_cn is empty

--- app/src/main/java/eu/kanade/tachiyomi/data/track/bangumi/BangumiApi.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/track/bangumi/BangumiApi.kt
@@ -110,9 +110,11 @@ class BangumiApi(
             0
         }
         val rating = obj["rating"]?.jsonObject?.get("score")?.jsonPrimitive?.doubleOrNull ?: -1.0
+        val nameCn = obj["name_cn"]?.jsonPrimitive?.contentOrNull
+        val name = obj["name"]!!.jsonPrimitive.content
         return TrackSearch.create(trackId).apply {
             remote_id = obj["id"]!!.jsonPrimitive.long
-            title = obj["name_cn"]!!.jsonPrimitive.content
+            title = if (!nameCn.isNullOrEmpty()) nameCn else name
             cover_url = coverUrl
             summary = obj["name"]!!.jsonPrimitive.content
             score = rating
