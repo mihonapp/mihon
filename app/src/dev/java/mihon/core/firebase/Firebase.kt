54e6e18131a58f8741a81a27739d6a29d60e021d
Roshan Varughese <40583749+Animeboynz@users.noreply.github.com>
2024-09-22 03:12:40 +1200

    Update Firebase.kt

--- app/src/dev/java/mihon/core/firebase/Firebase.kt
+++ app/src/dev/java/mihon/core/firebase/Firebase.kt
@@ -1,7 +1,8 @@
 
 import android.content.Context
 import eu.kanade.tachiyomi.core.security.SecurityPreferences
+import kotlinx.coroutines.CoroutineScope
 
 object Firebase {
-    fun setup(context: Context, preference: SecurityPreferences) = Unit
+    fun setup(context: Context, preference: SecurityPreferences, scope: CoroutineScope) = Unit
 }
