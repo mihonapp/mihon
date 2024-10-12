7c7af72f8cb12decc06b76c36852dcc54696236d
Roshan Varughese <40583749+Animeboynz@users.noreply.github.com>
2024-10-12 13:46:28 +1300

    Add option to opt out of Analytics and Crashlytics (#1237)

---
+++ app/src/dev/java/mihon/core/firebase/Firebase.kt
@@ -0,0 +1,9 @@
+package mihon.core.firebase
+
+import android.content.Context
+import eu.kanade.tachiyomi.core.security.PrivacyPreferences
+import kotlinx.coroutines.CoroutineScope
+
+object Firebase {
+    fun setup(context: Context, preference: PrivacyPreferences, scope: CoroutineScope) = Unit
+}
