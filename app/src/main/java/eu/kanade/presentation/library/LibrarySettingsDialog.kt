36c864d873469f171bcc7254e9ee385ab8c4cf7f
Jack Hamilton <jackham800@gmail.com>
2024-10-12 15:13:53 -0500

    Update app/src/main/java/eu/kanade/presentation/library/LibrarySettingsDialog.kt
    Co-authored-by: AntsyLich <59261191+AntsyLich@users.noreply.github.com>

--- app/src/main/java/eu/kanade/presentation/library/LibrarySettingsDialog.kt
+++ app/src/main/java/eu/kanade/presentation/library/LibrarySettingsDialog.kt
@@ -184,14 +184,10 @@ private fun ColumnScope.SortPage(
         MR.strings.action_sort_random to LibrarySort.Type.Random,
     ).plus(trackerSortOption).map { (titleRes, mode) ->
         if (mode == LibrarySort.Type.Random) {
-            val enabledIcon = if (sortingMode == LibrarySort.Type.Random) {
-                Icons.Default.Refresh
-            } else {
-                null
-            }
             BaseSortItem(
                 label = stringResource(titleRes),
-                icon = enabledIcon,
+                icon = Icons.Default.Refresh
+                    .takeIf { sortingMode == LibrarySort.Type.Random },
                 onClick = {
                     screenModel.setSort(category, mode, LibrarySort.Direction.Ascending)
                 },
