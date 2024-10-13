87db3f90dee8aa566038b46d0bdc975e715ab8f6
Roshan Varughese <40583749+Animeboynz@users.noreply.github.com>
2024-10-14 01:48:00 +1300

    Confirmation dialog when removing privately installed extensions (#1320)
    Co-authored-by: AntsyLich <59261191+AntsyLich@users.noreply.github.com>

--- app/src/main/java/eu/kanade/tachiyomi/ui/browse/extension/ExtensionsTab.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/browse/extension/ExtensionsTab.kt
@@ -1,8 +1,15 @@
 package eu.kanade.tachiyomi.ui.browse.extension
 
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
 import eu.kanade.presentation.browse.ExtensionScreen
@@ -12,6 +19,7 @@ import eu.kanade.presentation.more.settings.screen.browse.ExtensionReposScreen
 import eu.kanade.tachiyomi.extension.model.Extension
 import eu.kanade.tachiyomi.ui.browse.extension.details.ExtensionDetailsScreen
 import eu.kanade.tachiyomi.ui.webview.WebViewScreen
+import eu.kanade.tachiyomi.util.system.isPackageInstalled
 import kotlinx.collections.immutable.persistentListOf
 import tachiyomi.i18n.MR
 import tachiyomi.presentation.core.i18n.stringResource
@@ -21,7 +29,10 @@ fun extensionsTab(
     extensionsScreenModel: ExtensionsScreenModel,
 ): TabContent {
     val navigator = LocalNavigator.currentOrThrow
+    val context = LocalContext.current
+
     val state by extensionsScreenModel.state.collectAsState()
+    var privateExtensionToUninstall by remember { mutableStateOf<Extension?>(null) }
 
     return TabContent(
         titleRes = MR.strings.label_extensions,
@@ -45,7 +56,13 @@ fun extensionsTab(
                 onLongClickItem = { extension ->
                     when (extension) {
                         is Extension.Available -> extensionsScreenModel.installExtension(extension)
-                        else -> extensionsScreenModel.uninstallExtension(extension)
+                        else -> {
+                            if (context.isPackageInstalled(extension.pkgName)) {
+                                extensionsScreenModel.uninstallExtension(extension)
+                            } else {
+                                privateExtensionToUninstall = extension
+                            }
+                        }
                     }
                 },
                 onClickItemCancel = extensionsScreenModel::cancelInstallUpdateExtension,
@@ -68,6 +85,50 @@ fun extensionsTab(
                 onUpdateExtension = extensionsScreenModel::updateExtension,
                 onRefresh = extensionsScreenModel::findAvailableExtensions,
             )
+
+            privateExtensionToUninstall?.let { extension ->
+                ExtensionUninstallConfirmation(
+                    extensionName = extension.name,
+                    onClickConfirm = {
+                        extensionsScreenModel.uninstallExtension(extension)
+                    },
+                    onDismissRequest = {
+                        privateExtensionToUninstall = null
+                    },
+                )
+            }
         },
+    )
+}
+
+@Composable
+private fun ExtensionUninstallConfirmation(
+    extensionName: String,
+    onClickConfirm: () -> Unit,
+    onDismissRequest: () -> Unit,
+) {
+    AlertDialog(
+        title = {
+            Text(text = stringResource(MR.strings.ext_confirm_remove))
+        },
+        text = {
+            Text(text = stringResource(MR.strings.remove_private_extension_message, extensionName))
+        },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    onClickConfirm()
+                    onDismissRequest()
+                },
+            ) {
+                Text(text = stringResource(MR.strings.ext_remove))
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismissRequest) {
+                Text(text = stringResource(MR.strings.action_cancel))
+            }
+        },
+        onDismissRequest = onDismissRequest,
     )
 }
