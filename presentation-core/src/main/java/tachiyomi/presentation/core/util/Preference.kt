eb3bea8150ce9bf2320d15c879cbebaa6d51a4c6
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2024-10-19 19:53:06 +0600

    Revert "Tweak Preference.collectAsState"
    This reverts commit 3bddb5538528c19388e364d21e6a6c16487af759.
    
    Fixes #1341

--- presentation-core/src/main/java/tachiyomi/presentation/core/util/Preference.kt
+++ presentation-core/src/main/java/tachiyomi/presentation/core/util/Preference.kt
@@ -4,12 +4,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import kotlinx.coroutines.CoroutineScope
 import tachiyomi.core.common.preference.Preference
 
 @Composable
-fun <T> Preference<T>.collectAsState(scope: CoroutineScope = rememberCoroutineScope()): State<T> {
-    val flow = remember(this) { stateIn(scope) }
-    return flow.collectAsState()
+fun <T> Preference<T>.collectAsState(): State<T> {
+    val flow = remember(this) { changes() }
+    return flow.collectAsState(initial = get())
 }
