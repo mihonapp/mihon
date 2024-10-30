78f9a84b14e0ece988f80d61011f63c0f7e92a67
MajorTanya <39014446+MajorTanya@users.noreply.github.com>
2024-10-30 20:52:18 +0100

    Some improvements to Bangumi tracker search (#1396)
    In short:
    - fetch & show actual summary
    - fallback to "name" if "name_cn" is empty
    - request larger responseGroup to get & display the summary & rating
    - add type filter query param to make Bangumi filter, not us
    
    Previously, we only displayed the "name" in the summary area and used
    "name_cn" as the entry name. However, "name_cn" (Chinese name) can be
    an empty string at times, resulting in an awkward looking search
    result list where some, many, or even all the results have no title
    displayed and only show the "name" (Japanese name) in the summary
    area. This has been solved by using "name" as a fallback value should
    "name_cn" be empty.
    
    If a Chinese name is available, the original name is prepended to the
    summary with the addition "作品原名：" (meaning "original series title").
    
    By using the "responseGroup=large" query parameter, we can request
    the required data we need to display the actual summary for an entry
    and the entry's average rating.
    The "name" is prepended to the summary contents, if any exist, so it
    is still accessible for series identification if a "name_cn" exists
    too and was used for the result title.
    
    Adding the "type=1" filter query parameter means Bangumi will only
    return entries of type 1 ("book") instead of all types and Mihon
    needing to filter, resulting in potentially missed entry matches.

--- app/src/main/java/eu/kanade/tachiyomi/data/track/bangumi/dto/BGMSearch.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/track/bangumi/dto/BGMSearch.kt
@@ -17,6 +17,7 @@ data class BGMSearchItem(
     val nameCn: String,
     val name: String,
     val type: Int,
+    val summary: String?,
     val images: BGMSearchItemCovers?,
     @SerialName("eps_count")
     val epsCount: Long?,
@@ -25,9 +26,13 @@ data class BGMSearchItem(
 ) {
     fun toTrackSearch(trackId: Long): TrackSearch = TrackSearch.create(trackId).apply {
         remote_id = this@BGMSearchItem.id
-        title = nameCn
-        cover_url = images?.common ?: ""
-        summary = this@BGMSearchItem.name
+        title = nameCn.ifBlank { name }
+        cover_url = images?.common.orEmpty()
+        summary = if (nameCn.isNotBlank()) {
+            "作品原名：$name" + this@BGMSearchItem.summary?.let { "\n$it" }.orEmpty()
+        } else {
+            this@BGMSearchItem.summary.orEmpty()
+        }
         score = rating?.score ?: -1.0
         tracking_url = url
         total_chapters = epsCount ?: 0
