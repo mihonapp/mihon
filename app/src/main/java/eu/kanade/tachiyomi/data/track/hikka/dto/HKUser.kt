fcc8c76e65f43abcab74207dfe4ab1deb7f5d030
Lorg0n <lorgon.kv@gmail.com>
2025-01-02 01:47:14 +0300

    ref: deleted unnecessary fields in the HKUser

--- app/src/main/java/eu/kanade/tachiyomi/data/track/hikka/dto/HKUser.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/track/hikka/dto/HKUser.kt
@@ -5,12 +5,5 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class HKUser(
     val reference: String,
-    val updated: Long,
-    val created: Long,
-    val description: String,
     val username: String,
-    val cover: String,
-    val active: Boolean,
-    val avatar: String,
-    val role: String,
 )
