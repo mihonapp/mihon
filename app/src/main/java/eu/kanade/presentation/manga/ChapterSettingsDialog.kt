09bf54798c667669c87966d0604d75139b4b8c78
NGB-Was-Taken <76197326+NGB-Was-Taken@users.noreply.github.com>
2025-01-25 10:36:56 +0000

    Remove the pointless forceDownloaded function.

--- app/src/main/java/eu/kanade/presentation/manga/ChapterSettingsDialog.kt
+++ app/src/main/java/eu/kanade/presentation/manga/ChapterSettingsDialog.kt
@@ -26,8 +26,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import eu.kanade.domain.base.BasePreferences
 import eu.kanade.domain.manga.model.downloadedFilter
-import eu.kanade.domain.manga.model.forceDownloaded
 import eu.kanade.presentation.components.TabbedDialog
 import eu.kanade.presentation.components.TabbedDialogPaddings
 import kotlinx.collections.immutable.persistentListOf
@@ -40,6 +40,8 @@ import tachiyomi.presentation.core.components.SortItem
 import tachiyomi.presentation.core.components.TriStateItem
 import tachiyomi.presentation.core.i18n.stringResource
 import tachiyomi.presentation.core.theme.active
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
 
 @Composable
 fun ChapterSettingsDialog(
@@ -97,7 +99,7 @@ fun ChapterSettingsDialog(
                     FilterPage(
                         downloadFilter = manga?.downloadedFilter ?: TriState.DISABLED,
                         onDownloadFilterChanged = onDownloadFilterChanged
-                            .takeUnless { manga?.forceDownloaded() == true },
+                            .takeUnless { Injekt.get<BasePreferences>().downloadedOnly().get() == true },
                         unreadFilter = manga?.unreadFilter ?: TriState.DISABLED,
                         onUnreadFilterChanged = onUnreadFilterChanged,
                         bookmarkedFilter = manga?.bookmarkedFilter ?: TriState.DISABLED,
