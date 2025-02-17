3658652b320a5a80a9525409095ed7435cba1a69
fatotak <fatotaku@naver.com>
2025-02-17 17:52:06 +1100

    Changed variable name for lint error.

--- core/archive/src/main/kotlin/mihon/core/archive/TsZipWriter.kt
+++ core/archive/src/main/kotlin/mihon/core/archive/TsZipWriter.kt
@@ -5,7 +5,7 @@ import com.hippo.unifile.UniFile
 import java.io.Closeable
 
 class TsZipWriter(val context: Context, file: UniFile) : Closeable {
-    private var _zw = ZipWriter(context, file)
+    private val zw = ZipWriter(context, file)
 
     @get:Synchronized
     var count = 0
@@ -13,21 +13,21 @@ class TsZipWriter(val context: Context, file: UniFile) : Closeable {
 
     fun write(file: UniFile) {
         synchronized(this) {
-            _zw.write(file)
+            zw.write(file)
             count++
         }
     }
 
     fun write(filename: String, data: ByteArray) {
         synchronized(this) {
-            _zw.write(filename, data)
+            zw.write(filename, data)
             count++
         }
     }
 
     override fun close() {
         synchronized(this) {
-            _zw.close()
+            zw.close()
         }
     }
 }
