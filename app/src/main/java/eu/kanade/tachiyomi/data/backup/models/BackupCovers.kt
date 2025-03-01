1133e76feb83a49baa4fe9f355efc8c702f7a9de
Roshan Varughese <40583749+Animeboynz@users.noreply.github.com>
2025-03-01 15:21:13 +1300

    Allow for Export and Import of Custom Covers

---
+++ app/src/main/java/eu/kanade/tachiyomi/data/backup/models/BackupCovers.kt
@@ -0,0 +1,15 @@
+package eu.kanade.tachiyomi.data.backup.models
+
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.protobuf.ProtoNumber
+
+@Serializable
+data class BackupCovers(
+    @ProtoNumber(1) val mappings: List<BackupCover> = emptyList(),
+)
+
+@Serializable
+data class BackupCover(
+    @ProtoNumber(1) val mangaId: Long,
+    @ProtoNumber(2) val url: String
+)
