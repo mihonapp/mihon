a8d55166b58bb93b1ba89b525a54c8b605550e80
Lorg0n <lorgon.kv@gmail.com>
2025-03-07 02:28:21 +0300

    feat: add start date in manga search and improve type of content

--- app/src/main/java/eu/kanade/tachiyomi/data/track/hikka/dto/HKManga.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/track/hikka/dto/HKManga.kt
@@ -5,6 +5,8 @@ import eu.kanade.tachiyomi.data.track.hikka.stringToNumber
 import eu.kanade.tachiyomi.data.track.model.TrackSearch
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 @Serializable
 data class HKManga(
@@ -29,6 +31,8 @@ data class HKManga(
     val scoredBy: Int,
     val score: Double,
     val slug: String,
+    @SerialName("start_date")
+    val startDate: Long? = null
 ) {
     fun toTrack(trackId: Long): TrackSearch {
         return TrackSearch.create(trackId).apply {
@@ -39,8 +43,15 @@ data class HKManga(
             score = this@HKManga.score
             tracking_url = "${HikkaApi.BASE_URL}/manga/${this@HKManga.slug}"
             publishing_status = this@HKManga.status
-            publishing_type = "manga"
-            start_date = ""
+            publishing_type = this@HKManga.mediaType
+            startDate?.takeIf { it != 0L }?.let {
+                val outputDf = SimpleDateFormat("yyyy-MM-dd", Locale.US)
+                start_date = try {
+                    outputDf.format(it * 1000)
+                } catch (e: IllegalArgumentException) {
+                    ""
+                }
+            }
         }
     }
 }
