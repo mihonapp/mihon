134fcd93754ef8e136c82865a02754ef687e2a3e
arthur <16458204+ArthurKun021@users.noreply.github.com>
2025-04-03 07:31:28 +0800

    refactor: convert modifier extension functions to composable

--- presentation-core/src/main/java/tachiyomi/presentation/core/util/Modifier.kt
+++ presentation-core/src/main/java/tachiyomi/presentation/core/util/Modifier.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.isImeVisible
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -12,7 +13,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.focus.FocusRequester
@@ -24,16 +24,15 @@ import androidx.compose.ui.input.key.onPreviewKeyEvent
 import androidx.compose.ui.platform.LocalFocusManager
 import tachiyomi.presentation.core.components.material.SECONDARY_ALPHA
 
-fun Modifier.selectedBackground(isSelected: Boolean): Modifier = if (isSelected) {
-    composed {
+@Composable
+fun Modifier.selectedBackground(isSelected: Boolean): Modifier {
+    return if (isSelected) {
         val alpha = if (isSystemInDarkTheme()) 0.16f else 0.22f
         val color = MaterialTheme.colorScheme.secondary.copy(alpha = alpha)
-        Modifier.drawBehind {
-            drawRect(color)
-        }
+        this then Modifier.drawBehind { drawRect(color) }
+    } else {
+        this
     }
-} else {
-    this
 }
 
 fun Modifier.secondaryItemAlpha(): Modifier = this.alpha(SECONDARY_ALPHA)
@@ -60,6 +59,7 @@ fun Modifier.runOnEnterKeyPressed(action: () -> Unit): Modifier = this.onPreview
             action()
             true
         }
+
         else -> false
     }
 }
@@ -68,8 +68,9 @@ fun Modifier.runOnEnterKeyPressed(action: () -> Unit): Modifier = this.onPreview
  * For TextField on AppBar, this modifier will request focus
  * to the element the first time it's composed.
  */
-fun Modifier.showSoftKeyboard(show: Boolean): Modifier = if (show) {
-    composed {
+@Composable
+fun Modifier.showSoftKeyboard(show: Boolean): Modifier {
+    return if (show) {
         val focusRequester = remember { FocusRequester() }
         var openKeyboard by rememberSaveable { mutableStateOf(show) }
         LaunchedEffect(focusRequester) {
@@ -78,20 +79,20 @@ fun Modifier.showSoftKeyboard(show: Boolean): Modifier = if (show) {
                 openKeyboard = false
             }
         }
-
-        Modifier.focusRequester(focusRequester)
+        this then Modifier.focusRequester(focusRequester)
+    } else {
+        this
     }
-} else {
-    this
 }
 
 /**
  * For TextField, this modifier will clear focus when soft
  * keyboard is hidden.
  */
+@Composable
 fun Modifier.clearFocusOnSoftKeyboardHide(
     onFocusCleared: (() -> Unit)? = null,
-): Modifier = composed {
+): Modifier {
     var isFocused by remember { mutableStateOf(false) }
     var keyboardShowedSinceFocused by remember { mutableStateOf(false) }
     if (isFocused) {
@@ -107,7 +108,7 @@ fun Modifier.clearFocusOnSoftKeyboardHide(
         }
     }
 
-    Modifier.onFocusChanged {
+    return this then Modifier.onFocusChanged {
         if (isFocused != it.isFocused) {
             if (isFocused) {
                 keyboardShowedSinceFocused = false
