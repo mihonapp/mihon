4228b4f8519a22438d3991e9da2b1780b4c4d191
NarwhalHorns <onefailedgamer@gamil.com>
2025-04-18 08:20:53 +0100

    possible duplicates by subscribe to flow

--- domain/src/main/java/tachiyomi/domain/manga/repository/MangaRepository.kt
+++ domain/src/main/java/tachiyomi/domain/manga/repository/MangaRepository.kt
@@ -2,6 +2,7 @@ package tachiyomi.domain.manga.repository
 
 import kotlinx.coroutines.flow.Flow
 import tachiyomi.domain.library.model.LibraryManga
+import tachiyomi.domain.manga.model.DuplicateManga
 import tachiyomi.domain.manga.model.Manga
 import tachiyomi.domain.manga.model.MangaUpdate
 import tachiyomi.domain.manga.model.MangaWithChapterCount
@@ -13,6 +14,8 @@ interface MangaRepository {
     suspend fun getMangaByIdAsFlow(id: Long): Flow<Manga>
 
     suspend fun getMangaByUrlAndSourceId(url: String, sourceId: Long): Manga?
+
+    suspend fun getMangaByIdWithChapterCount(id: Long): MangaWithChapterCount
 
     fun getMangaByUrlAndSourceIdAsFlow(url: String, sourceId: Long): Flow<Manga?>
 
@@ -27,6 +30,8 @@ interface MangaRepository {
     fun getFavoritesBySourceId(sourceId: Long): Flow<List<Manga>>
 
     suspend fun getDuplicateLibraryManga(id: Long, searchString: String): List<MangaWithChapterCount>
+
+    suspend fun getAllDuplicateLibraryMangaAsFlow(fuzzy: Boolean = false): Flow<List<DuplicateManga>>
 
     suspend fun getHiddenDuplicates(manga: Manga): List<MangaWithChapterCount>
 
