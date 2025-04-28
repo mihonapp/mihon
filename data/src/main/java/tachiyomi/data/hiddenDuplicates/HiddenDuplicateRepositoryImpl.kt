1d282089ff0f737c011cbbf7b007cadb620e5ccc
NarwhalHorns <onefailedgamer@gamil.com>
2025-04-28 17:11:26 +0100

    remove redundant hidden dupe id

--- data/src/main/java/tachiyomi/data/hiddenDuplicates/HiddenDuplicateRepositoryImpl.kt
+++ data/src/main/java/tachiyomi/data/hiddenDuplicates/HiddenDuplicateRepositoryImpl.kt
@@ -21,12 +21,10 @@ class HiddenDuplicateRepositoryImpl(
     }
 
     private fun mapHiddenDuplicate(
-        id: Long,
         manga1Id: Long,
         manga2Id: Long,
     ): HiddenDuplicate {
         return HiddenDuplicate(
-            id = id,
             manga1Id = manga1Id,
             manga2Id = manga2Id,
         )
--- domain/src/main/java/tachiyomi/domain/manga/model/HiddenDuplicate.kt
+++ domain/src/main/java/tachiyomi/domain/manga/model/HiddenDuplicate.kt
@@ -1,7 +1,6 @@
 package tachiyomi.domain.manga.model
 
 data class HiddenDuplicate(
-    val id: Long,
     val manga1Id: Long,
     val manga2Id: Long,
 )
