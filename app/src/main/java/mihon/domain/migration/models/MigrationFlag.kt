77b37a1598e962525afe7d343ad9adc278cb316e
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2025-05-31 20:54:11 +0600

    final stuff

--- app/src/main/java/mihon/domain/migration/models/MigrationFlag.kt
+++ app/src/main/java/mihon/domain/migration/models/MigrationFlag.kt
@@ -3,16 +3,17 @@ package mihon.domain.migration.models
 enum class MigrationFlag(val flag: Int) {
     CHAPTER(0b00001),
     CATEGORY(0b00010),
+    // 0b00100 was used for manga trackers
     CUSTOM_COVER(0b01000),
     NOTES(0b100000),
     REMOVE_DOWNLOAD(0b10000),
     ;
 
     companion object {
-        fun fromBit(flag: Int): Set<MigrationFlag> {
+        fun fromBit(bit: Int): Set<MigrationFlag> {
             return buildSet {
                 entries.forEach { entry ->
-                    if (flag and entry.flag != 0) add(entry)
+                    if (bit and entry.flag != 0) add(entry)
                 }
             }
         }
