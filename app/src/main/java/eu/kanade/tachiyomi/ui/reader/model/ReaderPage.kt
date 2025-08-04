1be6873da9e27e56a0316b54fbcabadcdcf76055
Trevor Paley <10186337+TheUnlocked@users.noreply.github.com>
2025-08-03 21:15:55 -0700

    Use `ImageSource` to store image data during post-processing
    Currently the image post-processing pipeline involves lots of converting back and forth between bitmaps and streams. This change abstracts the data storage into an `ImageSource` type, which can be backed either by a `BufferedSource` or by a `Bitmap`. This allows us to avoid conversion to a bitmap when there is no post-processing, while also letting us keep images as a bitmap through the entire pipeline when there are multiple post-processing steps.

--- app/src/main/java/eu/kanade/tachiyomi/ui/reader/model/ReaderPage.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/reader/model/ReaderPage.kt
@@ -1,14 +1,64 @@
 package eu.kanade.tachiyomi.ui.reader.model
 
+import android.graphics.Bitmap
 import eu.kanade.tachiyomi.source.model.Page
+import okio.Buffer
+import tachiyomi.core.common.util.system.ImageUtil
+import tachiyomi.decoder.ImageDecoder
 import java.io.InputStream
 
 open class ReaderPage(
     index: Int,
     url: String = "",
     imageUrl: String? = null,
-    var stream: (() -> InputStream)? = null,
+    stream: (() -> InputStream)? = null,
 ) : Page(index, url, imageUrl, null) {
-
     open lateinit var chapter: ReaderChapter
+
+    private var _backingStream = stream
+    private var _backingBitmap: Bitmap? = null
+
+    /**
+     * A view of this page's image data represented as an [InputStream] thunk.
+     * @see [bitmap]
+     */
+    var stream: (() -> InputStream)?
+        get() = _backingStream ?: _backingBitmap?.let { {
+            Buffer().apply {
+                it.compress(Bitmap.CompressFormat.PNG, 100, outputStream())
+            }.inputStream()
+        } }
+        set(value) {
+            _backingStream = value
+            _backingBitmap = null
+        }
+
+    /**
+     * A view of this page's image data represented as a [Bitmap].
+     *
+     * This will not include any animation data that may be present in [stream], and setting
+     * this property will cause any existing animation data to be lost.
+     *
+     * @see [stream]
+     */
+    var bitmap: Bitmap?
+        get() = _backingBitmap ?: stream?.invoke()?.use {
+            ImageDecoder.newInstance(Buffer().readFrom(it).inputStream())?.run {
+                try {
+                    decode()
+                }
+                finally {
+                    recycle()
+                }
+            }
+        }
+        set(value) {
+            _backingStream = null
+            _backingBitmap = value
+        }
+
+    fun getImageSource(): ImageUtil.ImageSource? =
+        // Even if the backing data is already a bitmap, we need to clone it so _backingBitmap isn't consumed.
+        _backingBitmap?.let { ImageUtil.ImageSource.FromBitmap(Bitmap.createBitmap(it)) }
+            ?: _backingStream?.let { ImageUtil.ImageSource.FromBuffer(Buffer().readFrom(it())) }
 }
