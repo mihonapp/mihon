3c0cb7f732a73cf3359b99a469f094560aa27b45
Trevor Paley <10186337+TheUnlocked@users.noreply.github.com>
2025-08-18 19:15:44 -0700

    Do not wait for adjacent pages to load in SpreadFusionInterceptor
    The earlier approach favors correctness by not displaying pages until it's sure of whether a page is a spread or not. This adds significant latency to page loading however, and relies on preloading to work correctly. Eagerly assuming that pages are not spreads and letting them render when adjacent pages have not yet loaded will provide a worse experience when there are a large number of split spreads, but a significantly better experience when there are only a few, which is by far the more typical case.

--- app/src/main/java/eu/kanade/tachiyomi/ui/reader/loader/inteceptor/SpreadFusionInterceptor.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/reader/loader/inteceptor/SpreadFusionInterceptor.kt
@@ -1,22 +1,20 @@
 package eu.kanade.tachiyomi.ui.reader.loader.inteceptor
 
+import android.graphics.Bitmap
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.ui.reader.model.ReaderPage
 import io.github.theunlocked.mangavision.SpreadDetector
 import kotlinx.coroutines.sync.Mutex
 import tachiyomi.core.common.util.system.ImageUtil
 import tachiyomi.core.common.util.system.logcat
+import java.lang.ref.WeakReference
+import java.util.WeakHashMap
 
 class SpreadFusionInterceptor(pages: List<ReaderPage>, private val isLTR: Boolean) : PageLoaderInterceptor(pages) {
     private val checked = mutableSetOf<ReaderPage>()
     private val partOfSpread = mutableSetOf<ReaderPage>()
-    private val pendingNeighbors = mutableMapOf<ReaderPage, MutableSet<ReaderPage>>()
 
     override suspend fun onStatus(interceptedPage: ReaderPage) {
-        // Being in pendingNeighbors implies that the page is ready, so if a page is no longer ready, it should be
-        // removed from the map. If it becomes ready again later, it can be re-added as part of the regular logic.
-        pendingNeighbors.remove(interceptedPage)
-
         // This logic is non-deterministic if three or more pages in a row all form an extra-long spread, but that
         // scenario is rare and probably not worth accounting for here.
         // The `partOfSpread` set helps ensure that even if merging is non-deterministic, all pages will be
@@ -26,8 +24,6 @@ class SpreadFusionInterceptor(pages: List<ReaderPage>, private val isLTR: Boolea
 
         val prevPage = interceptedPages.getOrNull(interceptedPage.index - 1)
         val nextPage = interceptedPages.getOrNull(interceptedPage.index + 1)
-
-        val loadingAdjacentPages = mutableSetOf<ReaderPage>()
 
         if (interceptedPage.status == Page.State.Ready) {
             // `checked` and `partOfSpread` are used to avoid doing duplicate work, but they could also lead to a race
@@ -40,82 +36,61 @@ class SpreadFusionInterceptor(pages: List<ReaderPage>, private val isLTR: Boolea
                     return
                 }
 
-                if (nextPage != null && !checked.contains(interceptedPage)) {
-                    if (nextPage.status.isLoading()) {
-                        loadingAdjacentPages.add(nextPage)
-                    }
-                    else if (nextPage.status == Page.State.Ready && trySpread(interceptedPage, nextPage)) {
-                        pendingNeighbors.remove(nextPage)
-                        prevPage?.notifyNeighborIsReady(interceptedPage)
+                if (nextPage != null && nextPage.status == Page.State.Ready && !checked.contains(interceptedPage)) {
+                    if (trySpread(interceptedPage, nextPage)) {
                         return
                     }
                 }
 
-                if (prevPage != null && !checked.contains(prevPage)) {
-                    if (prevPage.status.isLoading()) {
-                        loadingAdjacentPages.add(prevPage)
-                    }
-                    else if (prevPage.status == Page.State.Ready && trySpread(prevPage, interceptedPage)) {
-                        pendingNeighbors.remove(prevPage)
-                        nextPage?.notifyNeighborIsReady(interceptedPage)
+                if (prevPage != null && prevPage.status == Page.State.Ready && !checked.contains(prevPage)) {
+                    if (trySpread(prevPage, interceptedPage)) {
                         return
                     }
                 }
             }
-        }
-
-        // If we're in any terminal state we'll let the surrounding pages load. Technically this is only valid if the
-        // current page is in the ready or skip state since if it's in an error state then on reload it could still
-        // create a spread, but in practice, it's better to load half a spread than leave the adjacent pages pending.
-        // If one of the pages gets changed to skip afterwards, it will just look a bit weird in the UI.
-        if (!interceptedPage.status.isLoading()) {
-            prevPage?.notifyNeighborIsReady(interceptedPage)
-            nextPage?.notifyNeighborIsReady(interceptedPage)
-        }
-
-        if (loadingAdjacentPages.size > 0) {
-            // Leave the page in a non-ready state because when a neighbor loads it could make this page part of a
-            // spread. We want to track this relationship so when the neighbor loads it can update this page.
-            logcat { "${interceptedPage.index} is waiting on ${loadingAdjacentPages.joinToString { it.index.toString() }}" }
-            pendingNeighbors[interceptedPage] = loadingAdjacentPages
-            return
         }
 
         super.onStatus(interceptedPage)
     }
 
-    private fun trySpread(page1: ReaderPage, page2: ReaderPage): Boolean {
+    private fun trySpread(
+        page1: ReaderPage,
+        page2: ReaderPage,
+    ): Boolean {
         checked.add(page1)
 
-        var bitmapFn1 = page1.bitmap ?: return false
-        var bitmapFn2 = page2.bitmap ?: return false
-
-        if (!isLTR) {
-            val tmp = bitmapFn1
-            bitmapFn1 = bitmapFn2
-            bitmapFn2 = tmp
+        val leftPage: ReaderPage
+        val rightPage: ReaderPage
+        if (isLTR) {
+            leftPage = page1
+            rightPage = page2
+        }
+        else {
+            leftPage = page2
+            rightPage = page1
         }
 
-        val bitmap1 = bitmapFn1()
-        val bitmap2 = bitmapFn2()
+        val leftBitmapFn = leftPage.bitmap ?: return false
+        val rightBitmapFn = rightPage.bitmap ?: return false
 
-        try {
-            if (!SpreadDetector.isSpread(bitmap1, bitmap2)) {
-                return false
-            }
-        }
-        finally {
-            // We don't want to hold these in memory. When it comes time to render the page, the bitmaps can be
-            // re-obtained for merging. In theory we could hold onto these for just the first time it loads since the
-            // image will generally be fetched immediately after the state changes to ready, but that adds a lot of
-            // complexity and it's probably not that big of a deal.
-            bitmap1.recycle()
-            bitmap2.recycle()
+        if (!SpreadDetector.isSpread(
+            getOrCreateWeaklyCachedBitmap(leftPage, leftBitmapFn),
+            getOrCreateWeaklyCachedBitmap(rightPage, rightBitmapFn),
+        )) {
+            return false
         }
 
         page1.output.apply {
-            bitmap = { ImageUtil.mergeHorizontal(bitmapFn1(), bitmapFn2()) }
-            status = Page.State.Ready
+            bitmap = { ImageUtil.mergeHorizontal(
+                getOrCreateWeaklyCachedBitmap(leftPage, leftBitmapFn),
+                getOrCreateWeaklyCachedBitmap(rightPage, rightBitmapFn),
+            ) }
+            if (status == Page.State.Ready) {
+                refreshStatus()
+            }
+            else {
+                status = Page.State.Ready
+            }
         }
         page2.output.apply {
             status = Page.State.Skip
@@ -124,26 +99,6 @@ class SpreadFusionInterceptor(pages: List<ReaderPage>, private val isLTR: Boolea
         partOfSpread.add(page2)
         logcat { "Detected spread on pages ${page1.index}-${page2.index}" }
         return true
-    }
-
-    private suspend fun ReaderPage.notifyNeighborIsReady(neighbor: ReaderPage) {
-        logcat { "${this.index} was notified ${neighbor.index} is ready" }
-        val waitingOn = pendingNeighbors[this] ?: return
-        if (waitingOn.remove(neighbor) && waitingOn.size == 0) {
-            pendingNeighbors.remove(this)
-            super.onStatus(this)
-        }
-    }
-
-    private fun Page.State.isLoading(): Boolean {
-        return when(this) {
-            Page.State.Queue -> true
-            Page.State.LoadPage -> true
-            Page.State.DownloadImage -> true
-            Page.State.Ready -> false
-            Page.State.Skip -> false
-            is Page.State.Error -> false
-        }
     }
 
     private val pageLocks = pages.map { Mutex() }
@@ -157,6 +112,30 @@ class SpreadFusionInterceptor(pages: List<ReaderPage>, private val isLTR: Boolea
         }
         finally {
             locks.forEach { it.unlock() }
+        }
+    }
+
+    private val weakBitmapCache = WeakHashMap<ReaderPage, Pair<Int, WeakReference<Bitmap>>>()
+
+    /**
+     * Gets the bitmap from [bitmapFn], weakly cached using [page] and its
+     * [imageHashCode][ReaderPage.imageHashCode] as the key. Both the key and value references are weak.
+     */
+    private fun getOrCreateWeaklyCachedBitmap(page: ReaderPage, bitmapFn: () -> Bitmap): Bitmap {
+        // Using weak references to cache data like this is generally not great practice, but the alternative is either
+        // keeping potentially large bitmaps in memory for an extended period of time (not good), or just not caching
+        // and instead re-generating the bitmaps every time (simpler, but slower). Even with the extremely short
+        // lifespan of cache entries, this can reduce the number of calls to bitmapFn by half or more.
+        return weakBitmapCache[page]?.let {
+            if (it.first != page.imageHashCode()) {
+                null
+            }
+            else {
+                it.second.get()
+            }
+        } ?: bitmapFn().let {
+            weakBitmapCache[page] = page.imageHashCode() to WeakReference(it)
+            it
         }
     }
 }
