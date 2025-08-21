4bc3b9f3b6fc87a9285ad3ca1aa8c81d8269d373
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2025-08-21 12:51:32 +0600

    Bump targetSdk to 35

--- buildSrc/src/main/kotlin/mihon/buildlogic/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/mihon/buildlogic/AndroidConfig.kt
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget as KotlinJvmTarget
 
 object AndroidConfig {
     const val COMPILE_SDK = 35
-    const val TARGET_SDK = 34
+    const val TARGET_SDK = 35
     const val MIN_SDK = 26
     const val NDK = "27.1.12297006"
     const val BUILD_TOOLS = "35.0.1"
