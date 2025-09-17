d3b7f7e55ff6609a2cddf1765205ba35c6c26b83
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2025-09-17 17:41:06 +0600

    Bump compile and target sdk

--- buildSrc/src/main/kotlin/mihon/buildlogic/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/mihon/buildlogic/AndroidConfig.kt
@@ -4,8 +4,8 @@ import org.gradle.api.JavaVersion as GradleJavaVersion
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget as KotlinJvmTarget
 
 object AndroidConfig {
-    const val COMPILE_SDK = 35
-    const val TARGET_SDK = 35
+    const val COMPILE_SDK = 36
+    const val TARGET_SDK = 36
     const val MIN_SDK = 26
     const val NDK = "27.1.12297006"
     const val BUILD_TOOLS = "35.0.1"
