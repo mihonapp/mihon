e07bd7af8c96f07579528616abfae350718de715
Cuong-Tran <cuongtran.tm@gmail.com>
2025-09-19 13:16:35 +0700

    fix(manga-restorer): Backward compatibility for old backup without `initialized`

--- app/src/main/java/eu/kanade/tachiyomi/data/backup/restore/restorers/MangaRestorer.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/backup/restore/restorers/MangaRestorer.kt
@@ -139,6 +139,7 @@ class MangaRestorer(
         manga: Manga,
     ): Manga {
         return manga.copy(
+            initialized = manga.initialized || manga.description != null,
             id = insertManga(manga),
         )
     }
