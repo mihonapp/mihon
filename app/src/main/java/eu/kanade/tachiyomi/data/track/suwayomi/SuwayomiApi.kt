2d1889dd59b438cce8a8e20a69c7304a96c81b4a
Constantin Piber <cp.piber@gmail.com>
2025-10-11 13:31:36 +0200

    Implement Suwayomi tracking via Extension

--- app/src/main/java/eu/kanade/tachiyomi/data/track/suwayomi/SuwayomiApi.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/track/suwayomi/SuwayomiApi.kt
@@ -1,5 +1,6 @@
 package eu.kanade.tachiyomi.data.track.suwayomi
 
+import android.app.Application
 import eu.kanade.tachiyomi.data.database.models.Track
 import eu.kanade.tachiyomi.data.track.model.TrackSearch
 import eu.kanade.tachiyomi.network.GET
@@ -13,6 +14,7 @@ import okhttp3.Headers
 import okhttp3.OkHttpClient
 import tachiyomi.core.common.util.lang.withIOContext
 import tachiyomi.domain.source.service.SourceManager
+import tachiyomi.i18n.MR
 import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
 import uy.kohesive.injekt.injectLazy
@@ -25,61 +27,37 @@ class SuwayomiApi(private val trackId: Long) {
 
     private val sourceManager: SourceManager by injectLazy()
     private val source: HttpSource by lazy { (sourceManager.get(sourceId) as HttpSource) }
+    private val suwayomiExt: SuwayomiExtensionInterface by lazy {
+        (sourceManager.get(sourceId) as? SuwayomiExtensionInterface)?.let { return@lazy it }
+        val context = Injekt.get<Application>()
+        throw NullPointerException(MR.strings.error_extension_mismatch.getString(context))
+    }
     private val client: OkHttpClient by lazy { source.client }
     private val headers: Headers by lazy { source.headers }
     private val baseUrl: String by lazy { source.baseUrl.trimEnd('/') }
 
     suspend fun getTrackSearch(trackUrl: String): TrackSearch = withIOContext {
-        val url = try {
-            // test if getting api url or manga id
-            val mangaId = trackUrl.toLong()
-            "$baseUrl/api/v1/manga/$mangaId"
-        } catch (e: NumberFormatException) {
-            trackUrl
-        }
-
-        val manga = with(json) {
-            client.newCall(GET("$url/full", headers))
-                .awaitSuccess()
-                .parseAs<MangaDataClass>()
-        }
-
+        val mangaId = trackUrl.getMangaId()
+        val manga = suwayomiExt.getTrackSearch(mangaId)
         TrackSearch.create(trackId).apply {
             title = manga.title
-            cover_url = "$url/thumbnail"
+            cover_url = "$baseUrl/${manga.thumbnailUrl}"
             summary = manga.description.orEmpty()
-            tracking_url = url
-            total_chapters = manga.chapterCount
+            tracking_url = mangaId.toString()
+            total_chapters = manga.chapters.toLong()
             publishing_status = manga.status
-            last_chapter_read = manga.lastChapterRead?.chapterNumber ?: 0.0
+            last_chapter_read = manga.latestReadChapter ?: 0.0
             status = when (manga.unreadCount) {
-                manga.chapterCount -> Suwayomi.UNREAD
-                0L -> Suwayomi.COMPLETED
+                manga.chapters -> Suwayomi.UNREAD
+                0 -> Suwayomi.COMPLETED
                 else -> Suwayomi.READING
             }
         }
     }
 
     suspend fun updateProgress(track: Track): Track {
-        val url = track.tracking_url
-        val chapters = with(json) {
-            client.newCall(GET("$url/chapters", headers))
-                .awaitSuccess()
-                .parseAs<List<ChapterDataClass>>()
-        }
-        val lastChapterIndex = chapters.first { it.chapterNumber == track.last_chapter_read }.index
-
-        client.newCall(
-            PUT(
-                "$url/chapter/$lastChapterIndex",
-                headers,
-                FormBody.Builder(Charset.forName("utf8"))
-                    .add("markPrevRead", "true")
-                    .add("read", "true")
-                    .build(),
-            ),
-        ).awaitSuccess()
-
+        val mangaId = track.tracking_url.getMangaId()
+        suwayomiExt.updateProgress(mangaId, track.last_chapter_read)
         return getTrackSearch(track.tracking_url)
     }
 
@@ -88,4 +66,9 @@ class SuwayomiApi(private val trackId: Long) {
         val bytes = MessageDigest.getInstance("MD5").digest(key.toByteArray())
         (0..7).map { bytes[it].toLong() and 0xff shl 8 * (7 - it) }.reduce(Long::or) and Long.MAX_VALUE
     }
+
+    private fun String.getMangaId(): Int =
+        // NOTE: Originally tracks were stored as API-v1 URLs of the form http://<base>/api/v1/manga/<mangaId>
+        //       Now, we store the ID directly, but for backwards compatibility support parsing the old format
+        this.substringAfterLast('/').toInt()
 }
