9b7cdc377f1ff1b9dd0b06a1ca2e5929f00dda69
NarwhalHorns <onefailedgamer@gamil.com>
2025-10-22 20:07:10 +0100

    some more stuff

--- app/src/main/java/eu/kanade/presentation/duplicates/PossibleDuplicatesScreen.kt
+++ app/src/main/java/eu/kanade/presentation/duplicates/PossibleDuplicatesScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -74,6 +75,7 @@ fun PossibleDuplicatesContent(
         }
 
         ScrollbarLazyColumn(
+            modifier = Modifier.fillMaxSize(),
             state = lazyListState,
             contentPadding = paddingValues,
             verticalArrangement = Arrangement.spacedBy(verticalListPadding),
