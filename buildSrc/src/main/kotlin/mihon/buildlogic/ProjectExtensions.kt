5fe7dd9f0612412afabd7646f89ba38b230fb7e7
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2025-12-13 00:18:48 +0600

    Use AGP provided NDK and Build Tools version

--- buildSrc/src/main/kotlin/mihon/buildlogic/ProjectExtensions.kt
+++ buildSrc/src/main/kotlin/mihon/buildlogic/ProjectExtensions.kt
@@ -26,13 +26,9 @@ val Project.libs get() = the<LibrariesForLibs>()
 internal fun Project.configureAndroid(commonExtension: CommonExtension<*, *, *, *, *, *>) {
     commonExtension.apply {
         compileSdk = AndroidConfig.COMPILE_SDK
-        buildToolsVersion = AndroidConfig.BUILD_TOOLS
 
         defaultConfig {
             minSdk = AndroidConfig.MIN_SDK
-            ndk {
-                version = AndroidConfig.NDK
-            }
         }
 
         compileOptions {
