17d538c656931aaa9bebd42c0f1f3af41751f3a5
MajorTanya <github@majortanya.eu>
2026-01-17 11:34:07 +0100

    Turn Scanlator filter into Switch

--- app/src/main/java/eu/kanade/presentation/updates/UpdatesFilterDialog.kt
+++ app/src/main/java/eu/kanade/presentation/updates/UpdatesFilterDialog.kt
@@ -1,12 +1,21 @@
 package eu.kanade.presentation.updates
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import eu.kanade.presentation.components.TabbedDialog
 import eu.kanade.presentation.components.TabbedDialogPaddings
@@ -15,8 +24,9 @@ import kotlinx.collections.immutable.persistentListOf
 import tachiyomi.core.common.preference.getAndSet
 import tachiyomi.domain.updates.service.UpdatesPreferences
 import tachiyomi.i18n.MR
-import tachiyomi.presentation.core.components.CheckboxItem
+import tachiyomi.presentation.core.components.SettingsItemsPaddings
 import tachiyomi.presentation.core.components.TriStateItem
+import tachiyomi.presentation.core.components.material.padding
 import tachiyomi.presentation.core.i18n.stringResource
 import tachiyomi.presentation.core.util.collectAsState
 
@@ -73,10 +83,29 @@ private fun ColumnScope.FilterSheet(
         onClick = { screenModel.toggleFilter(UpdatesPreferences::filterBookmarked) },
     )
 
+    HorizontalDivider(modifier = Modifier.padding(MaterialTheme.padding.small))
+
     val filterExcludedScanlators by screenModel.updatesPreferences.filterExcludedScanlators().collectAsState()
-    CheckboxItem(
-        label = stringResource(MR.strings.action_filter_excluded_scanlators),
-        checked = filterExcludedScanlators,
-        onClick = { screenModel.updatesPreferences.filterExcludedScanlators().getAndSet { !it } },
-    )
+
+    fun toggleScanlatorFilter() = screenModel.updatesPreferences.filterExcludedScanlators().getAndSet { !it }
+
+    Row(
+        modifier = Modifier
+            .clickable { toggleScanlatorFilter() }
+            .fillMaxWidth()
+            .padding(horizontal = SettingsItemsPaddings.Horizontal),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Text(
+            text = stringResource(MR.strings.action_filter_excluded_scanlators),
+            color = MaterialTheme.colorScheme.onSurface,
+            style = MaterialTheme.typography.bodyMedium,
+        )
+
+        Switch(
+            checked = filterExcludedScanlators,
+            onCheckedChange = { toggleScanlatorFilter() },
+        )
+    }
 }
