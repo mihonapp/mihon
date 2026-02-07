9a5af5e27f051a3909500521b7c99f7ad78a348a
mis0suppe <misosuppe@proton.me>
2026-02-07 21:54:01 +0000

    chore: reset versioning to v0.1.1 and update fork branding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
     defaultConfig {
         applicationId = "app.mihon.ds"
 
-        versionCode = 16
-        versionName = "0.19.3"
+        versionCode = 17
+        versionName = "0.1.1"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
