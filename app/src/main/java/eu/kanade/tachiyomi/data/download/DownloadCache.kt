e59d1ed1863558be3c9cfbddea0512d0cdaf981d
Luca Auer <lolle2000.la@gmail.com>
2026-02-17 20:34:45 +0100

    Revert "Do early returns to prevent accidental double dips"
    This reverts commit ce86dbccf89cfc4126f4bbff50b161d9dc82a23e.

--- app/src/main/java/eu/kanade/tachiyomi/data/download/DownloadCache.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/download/DownloadCache.kt
@@ -103,9 +103,7 @@ class DownloadCache(
     init {
         // Attempt to read cache file
         scope.launch {
-            if (renewalJob?.isActive == true) return@launch
             rootDownloadsDirMutex.withLock {
-                if (renewalJob?.isActive == true) return@withLock
                 try {
                     if (diskCacheFile.exists()) {
                         val diskCache = diskCacheFile.inputStream().use {
