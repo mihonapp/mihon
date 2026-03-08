649c3950f5ba71bffac6f03b492ad177e602bf0c
MajorTanya <github@majortanya.eu>
2026-03-08 22:41:02 +0100

    Add support for MangaBaka
    Note: missing a client ID which should be added by the project owner.

---
+++ app/src/main/java/eu/kanade/tachiyomi/data/track/mangabaka/dto/MangaBakaListEntry.kt
@@ -0,0 +1,35 @@
+package eu.kanade.tachiyomi.data.track.mangabaka.dto
+
+import eu.kanade.tachiyomi.data.track.mangabaka.MangaBaka
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MangaBakaListResult(
+    val data: MangaBakaListEntry,
+)
+
+@Serializable
+data class MangaBakaListEntry(
+    val state: String,
+    @SerialName("start_date")
+    val startDate: String?,
+    @SerialName("finish_name")
+    val finishDate: String?,
+    @SerialName("is_private")
+    val isPrivate: Boolean,
+    @SerialName("progress_chapter")
+    val progressChapter: Double?,
+    val rating: Long?,
+) {
+    fun getStatus(): Long = when (state) {
+        "considering" -> MangaBaka.CONSIDERING
+        "completed" -> MangaBaka.COMPLETED
+        "dropped" -> MangaBaka.DROPPED
+        "paused" -> MangaBaka.PAUSED
+        "plan_to_read" -> MangaBaka.PLAN_TO_READ
+        "reading" -> MangaBaka.READING
+        "rereading" -> MangaBaka.REREADING
+        else -> throw NotImplementedError("Unknown status: $state")
+    }
+}
