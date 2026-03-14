b6d84abb4de81dd61bd43ecfcb8bb16fe2314672
neo <df2023-kagura2aka@protonmail.com>
2026-03-14 19:40:59 +0900

    chore: remove unused height import (Step 4)

--- app/src/main/java/eu/kanade/presentation/library/components/CategoryOverlayDialog.kt
+++ app/src/main/java/eu/kanade/presentation/library/components/CategoryOverlayDialog.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
