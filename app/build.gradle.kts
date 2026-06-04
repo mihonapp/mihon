a1ad33baef439f0578bd86edd6e18863d3ad494d
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2026-06-05 00:08:21 +0600

    Remove redundant compiler argument for annotation default target

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -162,7 +162,6 @@ kotlin {
             "-opt-in=kotlinx.coroutines.FlowPreview",
             "-opt-in=kotlinx.coroutines.InternalCoroutinesApi",
             "-opt-in=kotlinx.serialization.ExperimentalSerializationApi",
-            "-Xannotation-default-target=param-property",
         )
     }
 }
