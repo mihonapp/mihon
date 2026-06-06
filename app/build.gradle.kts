2070b0b12fc5d31768004a1ffe6efcd902f66516
w <wvvwvvvvwvvw@gmail.com>
2026-06-06 12:39:40 -0700

    Add high quality webgpu renderer

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -249,6 +249,7 @@ dependencies {
         exclude(module = "image-decoder")
     }
     implementation(libs.image.decoder)
+    implementation(libs.webgpuviewer)
 
     // UI libraries
     implementation(libs.material)
