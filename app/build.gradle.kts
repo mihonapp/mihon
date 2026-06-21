1cb38a1c3399e47fcb0eec39043d8925acd10e21
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2026-06-22 00:51:22 +0600

    Bump version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
     defaultConfig {
         applicationId = "app.mihon"
 
-        versionCode = 22
+        versionCode = 23
         versionName = "0.19.9"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getLatestCommitCount()}\"")
