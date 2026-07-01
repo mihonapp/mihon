544a9aa48a703f1d6fd1f2c72591f39802eea5b0
w <wvvwvvvvwvvw@gmail.com>
2026-07-01 14:37:06 -0700

    Fix continuous scrolling when going back

--- app/src/main/java/eu/kanade/tachiyomi/ui/reader/viewer/webgpu/WebGpuViewerContinuous.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/reader/viewer/webgpu/WebGpuViewerContinuous.kt
@@ -2,9 +2,15 @@ package eu.kanade.tachiyomi.ui.reader.viewer.webgpu
 
 import ca.mpreg.webgpuviewer.ImageViewContinuous
 import eu.kanade.tachiyomi.ui.reader.ReaderActivity
+import eu.kanade.tachiyomi.ui.reader.model.ReaderPage
 
 class WebGpuViewerContinuous(activity: ReaderActivity) :
     WebGpuViewer(activity, isReversed = false, isVertical = true, pager = ImageViewContinuous(activity)) {
 
     override fun updateTransitionAnimation() {}
+
+    override fun moveToPage(page: ReaderPage) {
+        super.moveToPage(page)
+        (pager as ImageViewContinuous).state.scrollY = 0f
+    }
 }
