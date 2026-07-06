6d80298aa0edf9c1917e6b16d0eed7fbb7a3ba3c
w <wvvwvvvvwvvw@gmail.com>
2026-07-05 17:33:26 -0700

    Add navigation for continuous

--- app/src/main/java/eu/kanade/tachiyomi/ui/reader/viewer/webgpu/WebGpuViewerContinuous.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/reader/viewer/webgpu/WebGpuViewerContinuous.kt
@@ -1,13 +1,36 @@
 package eu.kanade.tachiyomi.ui.reader.viewer.webgpu
 
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animate
+import androidx.compose.animation.core.spring
 import ca.mpreg.webgpuviewer.ImageViewContinuous
 import eu.kanade.tachiyomi.ui.reader.ReaderActivity
 import eu.kanade.tachiyomi.ui.reader.model.ReaderPage
+import kotlinx.coroutines.launch
 
 class WebGpuViewerContinuous(activity: ReaderActivity) :
     WebGpuViewer(activity, isReversed = false, isVertical = true, pager = ImageViewContinuous(activity)) {
 
     override fun updateTransitionAnimation() {}
+
+    private fun scrollByHalfPage(direction: Int) {
+        val state = (pager as ImageViewContinuous).state
+        val totalDistance = direction * state.height / 2f
+
+        state.animationJob?.cancel()
+        state.animationJob = state.scope?.launch {
+            var lastValue = 0f
+            animate(0f, totalDistance, animationSpec = spring(stiffness = Spring.StiffnessMediumLow)) { value, _ ->
+                state.scrollBy(value - lastValue)
+                lastValue = value
+                state.invalidate()
+            }
+        }
+    }
+
+    override fun moveRight() = scrollByHalfPage(1)
+
+    override fun moveLeft() = scrollByHalfPage(-1)
 
     override fun moveToPage(page: ReaderPage) {
         super.moveToPage(page)
