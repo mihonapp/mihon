850a3d7047c831f246b618d436c0321ada42bb1b
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2026-07-23 19:11:01 +0600

    Convert :source-local to android library module

--- source-local/src/main/kotlin/tachiyomi/source/local/io/LocalSourceFileSystem.kt
+++ source-local/src/main/kotlin/tachiyomi/source/local/io/LocalSourceFileSystem.kt
@@ -3,25 +3,25 @@ package tachiyomi.source.local.io
 import com.hippo.unifile.UniFile
 import tachiyomi.domain.storage.service.StorageManager
 
-actual class LocalSourceFileSystem(
+class LocalSourceFileSystem(
     private val storageManager: StorageManager,
 ) {
 
-    actual fun getBaseDirectory(): UniFile? {
+    fun getBaseDirectory(): UniFile? {
         return storageManager.getLocalSourceDirectory()
     }
 
-    actual fun getFilesInBaseDirectory(): List<UniFile> {
+    fun getFilesInBaseDirectory(): List<UniFile> {
         return getBaseDirectory()?.listFiles().orEmpty().toList()
     }
 
-    actual fun getMangaDirectory(name: String): UniFile? {
+    fun getMangaDirectory(name: String): UniFile? {
         return getBaseDirectory()
             ?.findFile(name)
             ?.takeIf { it.isDirectory }
     }
 
-    actual fun getFilesInMangaDirectory(name: String): List<UniFile> {
+    fun getFilesInMangaDirectory(name: String): List<UniFile> {
         return getMangaDirectory(name)?.listFiles().orEmpty().toList()
     }
 }
