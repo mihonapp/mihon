d0f770b73fe5460828ef7aaf173bad38a92b8ccd
hell1231 <81326807+hell1231@users.noreply.github.com>
2026-07-27 14:38:04 +0000

    Bump version to 0.20.2 for release build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
     defaultConfig {
         applicationId = "app.mihon"
 
-        versionCode = 26
-        versionName = "0.20.1"
+        versionCode = 27
+        versionName = "0.20.2"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getLatestCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getLatestCommitSha()}\"")
