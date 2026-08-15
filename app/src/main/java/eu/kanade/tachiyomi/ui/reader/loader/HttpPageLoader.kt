838bd465729f6f41632eba2513c75ce6cbb6691d
Rodrigo Oliveira <32792889+rodrigdoliveira@users.noreply.github.com>
2026-08-15 15:36:35 +0200

    Retry pages after reader decode failures

--- app/src/main/java/eu/kanade/tachiyomi/ui/reader/loader/HttpPageLoader.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/reader/loader/HttpPageLoader.kt
@@ -118,9 +118,7 @@ internal class HttpPageLoader(
      * Retries a page. This method is only called from user interaction on the viewer.
      */
     override fun retryPage(page: ReaderPage) {
-        if (page.status is Page.State.Error) {
-            page.status = Page.State.Queue
-        }
+        page.status = Page.State.Queue
         queue.offer(PriorityPage(page, PriorityPage.RETRY))
     }
 
