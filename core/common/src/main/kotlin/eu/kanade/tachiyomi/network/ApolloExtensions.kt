3b813cc799f10a18f13091537f128bcace8f6394
MajorTanya <github@majortanya.eu>
2026-08-25 07:23:20 +0200

    Delete obsolete ApolloCall.awaitSuccess

--- core/common/src/main/kotlin/eu/kanade/tachiyomi/network/ApolloExtensions.kt
+++ core/common/src/main/kotlin/eu/kanade/tachiyomi/network/ApolloExtensions.kt
@@ -1,23 +1,10 @@
 package eu.kanade.tachiyomi.network
 
-import com.apollographql.apollo.ApolloCall
 import com.apollographql.apollo.api.ApolloResponse
 import com.apollographql.apollo.api.Operation
 import com.apollographql.apollo.exception.ApolloHttpException
 import logcat.LogPriority
 import tachiyomi.core.common.util.system.logcat
-
-/**
- * Similar to [okhttp3.Call.awaitSuccess] and wraps [ApolloHttpException] with [HttpException]
- */
-suspend fun <D : Operation.Data> ApolloCall<D>.awaitSuccess(): ApolloResponse<D> {
-    val callStack = Exception().stackTrace.run { copyOfRange(1, size) }
-    return try {
-        execute()
-    } catch (e: ApolloHttpException) {
-        throw HttpException(e.statusCode).apply { stackTrace = callStack }
-    }
-}
 
 /**
  * Returns the result of calling [transform] with [data] if the GraphQL response had data present without errors,
