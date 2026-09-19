01e3f8c60557d89b6cd9d28c11f41d676d0ab5d9
MajorTanya <39014446+MajorTanya@users.noreply.github.com>
2026-09-19 20:35:25 +0200

    Add refresh button to tracker for new profile info (#3828)
    * Add refresh button to tracker for new profile info
    
    As-is, allows a user to manually refresh their current user data from
    the tracker in order to update Mihon's knowledge of remote settings
    (e.g. nickname changes, AL/MB/Kitsu score types, etc.).
    
    * Catch errors to prevent app crashing
    
    Surfaces errors as toasts in the same way library title refreshes do.
    
    * spotless
    
    * [skip ci] Remove leftover debugging logs
    
    * Fix Kitsu user attribute access
    
    * Spotless
    
    * Lowercase Kitsu score type on refresh
    
    Maybe prevent needing another #3887 for this.
    
    * Center tracker vertically name if not logged in
    
    Looks much better than the name being stuck at the top with empty
    space below it.
    
    * Don't show Refresh button for Enhanced trackers
    
    * [skip ci] Add changelog entry

--- app/src/main/java/eu/kanade/test/DummyTracker.kt
+++ app/src/main/java/eu/kanade/test/DummyTracker.kt
@@ -2,9 +2,15 @@ package eu.kanade.test
 
 import dev.icerock.moko.resources.StringResource
 import eu.kanade.tachiyomi.R
+import eu.kanade.tachiyomi.data.track.RefreshResult
 import eu.kanade.tachiyomi.data.track.Tracker
 import eu.kanade.tachiyomi.data.track.model.TrackSearch
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.flowOf
 import okhttp3.OkHttpClient
 import tachiyomi.domain.track.model.Track
@@ -17,6 +23,8 @@ data class DummyTracker(
     override val supportsPrivateTracking: Boolean = false,
     override val isLoggedIn: Boolean = false,
     override val isLoggedInFlow: Flow<Boolean> = flowOf(false),
+    override val isRefreshingFlow: StateFlow<Boolean> = MutableStateFlow(false),
+    override val refreshResultFlow: SharedFlow<RefreshResult> = MutableSharedFlow<RefreshResult>().asSharedFlow(),
     val valLogo: Int = R.drawable.brand_anilist,
     val valStatuses: List<Long> = (1L..6L).toList(),
     val valReadingStatus: Long = 1L,
@@ -74,6 +82,8 @@ data class DummyTracker(
     override suspend fun refresh(
         track: eu.kanade.tachiyomi.data.database.models.Track,
     ): eu.kanade.tachiyomi.data.database.models.Track = track
+
+    override suspend fun refreshUser() = Unit
 
     override suspend fun login(username: String, password: String) = Unit
 
